51914fe04c77855bbb4d3bbfc709a59962262aac
jve0902 <jay090218@gmail.com>
2020-11-03 16:47:26 +0100

    Add client_portal handouts

--- server/server.js
+++ server/server.js
@@ -63,6 +63,7 @@ app.use(baseAPIPath, require("./app/routes/patient/home.routes"));
 app.use(baseAPIPath, require("./app/routes/patient/password-reset.routes"));
 app.use(baseAPIPath, require("./app/routes/patient/messages.routes"));
 app.use(baseAPIPath, require("./app/routes/patient/encounters.routes"));
+app.use(baseAPIPath, require("./app/routes/patient/handouts.routes"));
 
 app.use(baseAPIPath, require("./app/routes/myself.routes"));
 
