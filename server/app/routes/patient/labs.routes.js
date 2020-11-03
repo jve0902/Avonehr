c904a56c91d9234c8a3c4991143a7f40b948c6a3
jve0902 <jay090218@gmail.com>
2020-11-03 16:55:53 +0100

    Add labs routes

---
+++ server/app/routes/patient/labs.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { authJwt } = require("../../middlewares");
+const lanbsController = require("../../controllers/patient/labs.controller");
+
+const router = express.Router();
+
+router.get(
+  "/client-portal/labs",
+  [authJwt.verifyToken],
+  lanbsController.getAlllabs
+);
+router.put(
+  "/client-portal/labs/:labId",
+  [authJwt.verifyToken],
+  lanbsController.updateLab
+);
+
+module.exports = router;
