79e2dcced7856c5aea9f4fc94817421b9291532f
jve0902 <jay090218@gmail.com>
2020-11-04 08:18:35 +0100

    Add airbnb base for client

--- client/src/components/email/VerificationMessage.js
+++ client/src/components/email/VerificationMessage.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 import Alert from "@material-ui/lab/Alert";
+import PropTypes from "prop-types";
 
-const VerificationMessage = ({ severity, message }) => {
-  return <Alert severity={severity || "error"}>{message}</Alert>;
+const VerificationMessage = ({ severity, message }) => (
+  <Alert severity={severity}>{message}</Alert>
+);
+
+VerificationMessage.defaultProps = {
+  severity: "error",
+};
+
+VerificationMessage.propTypes = {
+  severity: PropTypes.string,
+  message: PropTypes.string.isRequired,
 };
 
 export default VerificationMessage;
--- client/src/components/email/VerificationSuccess.js
+++ client/src/components/email/VerificationSuccess.js
@@ -4,6 +4,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,9 +12,9 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     "& p": {
       fontSize: "16px",
-      lineHeight: "24px"
-    }
-  }
+      lineHeight: "24px",
+    },
+  },
 }));
 
 const VerificationSuccess = ({ isEmailVerified }) => {
@@ -23,25 +24,31 @@ const VerificationSuccess = ({ isEmailVerified }) => {
       <CardContent>
         {isEmailVerified ? (
           <p>
-            Your can login here:{" "}
+            Your can login here:
+            {" "}
             <Link href={`${process.env.REACT_APP_SITE_URL}login_client`}>
               {`${process.env.REACT_APP_SITE_URL}login_client`}
             </Link>
           </p>
         ) : (
-          <React.Fragment>
+          <>
             <p>Thank you for confirming your email address. </p>
             <p>
-              Your login page would be{" "}
+              Your login page would be
+              {" "}
               <Link href={`${process.env.REACT_APP_SITE_URL}login_client`}>
                 {`${process.env.REACT_APP_SITE_URL}login_client`}
               </Link>
             </p>
-          </React.Fragment>
+          </>
         )}
       </CardContent>
     </Card>
   );
+};
+
+VerificationSuccess.propTypes = {
+  isEmailVerified: PropTypes.bool.isRequired,
 };
 
 export default VerificationSuccess;
