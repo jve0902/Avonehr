6ed0ee6179dc9840af3ce37ee4e08be5b5283c96
jve0902 <jay090218@gmail.com>
2020-11-07 23:11:34 +0500

    Admin notes prop types fixed

--- client/src/screens/Patient/components/AdminNotes/History.js
+++ client/src/screens/Patient/components/AdminNotes/History.js
@@ -8,11 +8,12 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import moment from "moment";
+import PropTypes from "prop-types";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   tableContainer: {
-    minWidth: 650
-  }
+    minWidth: 650,
+  },
 }));
 
 const StyledTableCell = withStyles((theme) => ({
@@ -20,27 +21,27 @@ const StyledTableCell = withStyles((theme) => ({
     backgroundColor: theme.palette.grey,
     color: theme.palette.grey,
     fontSize: "12px",
-    fontWeight: 700
+    fontWeight: 700,
   },
   body: {
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
     fontSize: 14,
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover
+      backgroundColor: theme.palette.action.hover,
     },
     "& th": {
-      fontSize: 12
+      fontSize: 12,
     },
     "& td": {
       fontSize: 12,
-      height: "50px"
-    }
-  }
+      height: "50px",
+    },
+  },
 }))(TableRow);
 
 const AdminNotesHistory = (props) => {
@@ -58,10 +59,10 @@ const AdminNotesHistory = (props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {!!data &&
-            data.length &&
-            data.map((row, index) => (
-              <StyledTableRow key={`${row.created}_${index}`}>
+          {!!data
+            && data.length
+            && data.map((row) => (
+              <StyledTableRow key={row.created}>
                 <TableCell component="th" scope="row">
                   {moment(row.created).format("MMM D YYYY")}
                 </TableCell>
@@ -73,6 +74,10 @@ const AdminNotesHistory = (props) => {
       </Table>
     </TableContainer>
   );
+};
+
+AdminNotesHistory.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default AdminNotesHistory;
