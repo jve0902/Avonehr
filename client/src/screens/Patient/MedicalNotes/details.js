171c755cdd113631798f2663b96d140c6c64357a
jve0902 <jay090218@gmail.com>
2020-11-08 11:41:52 +0500

    Medical notes prop types fixed

--- client/src/screens/Patient/MedicalNotes/details.js
+++ client/src/screens/Patient/MedicalNotes/details.js
@@ -8,13 +8,11 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import moment from "moment";
+import PropTypes from "prop-types";
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    padding: 9
-  },
+const useStyles = makeStyles(() => ({
   tableContainer: {
-    minWidth: 650
+    minWidth: 650,
   },
   actions: {
     textAlign: "center",
@@ -22,9 +20,9 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     border: "none",
     "& button": {
-      fontSize: "12px"
-    }
-  }
+      fontSize: "12px",
+    },
+  },
 }));
 
 const StyledTableCell = withStyles((theme) => ({
@@ -32,27 +30,27 @@ const StyledTableCell = withStyles((theme) => ({
     backgroundColor: theme.palette.grey,
     color: theme.palette.grey,
     fontSize: "12px",
-    fontWeight: 700
+    fontWeight: 700,
   },
   body: {
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
     fontSize: 14,
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover
+      backgroundColor: theme.palette.action.hover,
     },
     "& th": {
-      fontSize: 12
+      fontSize: 12,
     },
     "& td": {
       fontSize: 12,
-      height: "50px"
-    }
-  }
+      height: "50px",
+    },
+  },
 }))(TableRow);
 
 const MedicalNotesDetails = (props) => {
@@ -70,8 +68,8 @@ const MedicalNotesDetails = (props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row, index) => (
-            <StyledTableRow key={`${row.created}_${index}`}>
+          {data.map((row) => (
+            <StyledTableRow key={`${row.created}_${row.name}`}>
               <TableCell component="th" scope="row">
                 {moment(row.created).format("MMM D YYYY")}
               </TableCell>
@@ -83,6 +81,10 @@ const MedicalNotesDetails = (props) => {
       </Table>
     </TableContainer>
   );
+};
+
+MedicalNotesDetails.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MedicalNotesDetails;
