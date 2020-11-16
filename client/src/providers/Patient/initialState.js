0b36727f1b1cac486e9faccbd6e814ac39774751
jve0902 <jay090218@gmail.com>
2020-11-16 19:50:06 +0500

    providers/Patient/initialState.js

---
+++ client/src/providers/Patient/initialState.js
@@ -0,0 +1,77 @@
+const initialState = {
+  patientInfo: {
+    data: {},
+    history: [],
+    editDialog: false,
+    historyDialog: false,
+  },
+  adminNotes: {
+    data: [],
+    editForm: false,
+    historyDialog: false,
+  },
+  forms: {
+    data: [],
+    viewDialog: false,
+    expandDialog: false,
+  },
+  handouts: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  documents: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  encounters: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  medicalNotes: {
+    data: [],
+    editForm: false,
+    historyDialog: false,
+  },
+  allergies: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  messages: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  requisitions: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  tests: {
+    data: [],
+    expandDialog: false,
+  },
+  diagnoses: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+    status: true,
+  },
+  medications: {
+    data: [],
+    newDialog: false,
+    expandDialog: false,
+  },
+  billing: {
+    data: [],
+    balance: 0,
+    newDialog: false,
+    expandDialog: false,
+    newTransactionDialog: false,
+  },
+};
+
+export default initialState;
