aef39fa444cadf86e73cb04cdc3d4058fb370143
jve0902 <jay090218@gmail.com>
2020-11-22 22:01:03 +0500

    appointments page for patient-portal

--- client/src/screens/patient-portal/Appointments/Appointments.js
+++ client/src/screens/patient-portal/Appointments/Appointments.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { makeStyles } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
+import {
+  makeStyles, Grid, TextField, Typography, MenuItem, Button,
+} from "@material-ui/core";
+import moment from "moment";
+
+import { timings, appointments } from "../../../static/patient-portal/appointments";
+import Calendar from "./Calendar";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,10 +17,50 @@ const useStyles = makeStyles((theme) => ({
   title: {
     paddingBottom: theme.spacing(1),
   },
+  inputFix: {
+    marginBottom: theme.spacing(1),
+  },
+  submitBtn: {
+    marginTop: theme.spacing(2),
+    minWidth: 120,
+  },
+  calendarContainer: {
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(1),
+    borderRadius: 5,
+    boxShadow: "-1px 0px 19px -4px rgba(0,17,1,0.45)",
+  },
+  timingBox: {
+    marginBottom: theme.spacing(2),
+    minHeight: 50,
+  },
+  currentDate: {
+    paddingBottom: theme.spacing(5),
+  },
 }));
 
-export default function Appointments() {
+const Appointments = () => {
   const classes = useStyles();
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [userSelection, setUserSelection] = useState({
+    practitioner: "",
+    appointmentType: "",
+    date: null,
+    time: null,
+  });
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setShowCalendar(true);
+  };
+
+  const calendarSelectionHandler = (type, value) => {
+    setUserSelection({
+      ...userSelection,
+      [type]: value,
+    });
+  };
+
   return (
     <div className={classes.root}>
       <Typography
@@ -25,6 +71,118 @@ export default function Appointments() {
       >
         Appointments
       </Typography>
+      {!showCalendar
+        ? (
+          <Grid item lg={3} md={4} sm={6} xs={12}>
+            <form onSubmit={onFormSubmit}>
+              <TextField
+                select
+                required
+                variant="outlined"
+                label="Select Practitioner"
+                margin="dense"
+                fullWidth
+                value={userSelection.practitioner}
+                className={classes.inputFix}
+                onChange={(e) => calendarSelectionHandler("practitioner", e.target.value)}
+              >
+                {appointments.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                select
+                required
+                variant="outlined"
+                label="Select Appointment Type"
+                margin="dense"
+                fullWidth
+                value={userSelection.appointmentType}
+                className={classes.inputFix}
+                onChange={(e) => calendarSelectionHandler("appointmentType", e.target.value)}
+              >
+                {appointments.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Grid
+                container
+                justify="center"
+              >
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  className={classes.submitBtn}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </form>
+          </Grid>
+        )
+        : (
+          <>
+            <Typography
+              variant="h5"
+              color="textPrimary"
+              className={classes.title}
+            >
+              Please select a date and time
+            </Typography>
+            <Grid item lg={8} md={8} sm={12} xs={12}>
+              <Grid
+                container
+                className={classes.calendarContainer}
+                spacing={3}
+              >
+                <Grid item lg={9} md={9} sm={9} xs={9}>
+                  <Calendar
+                    events={
+                      userSelection.date
+                        ? [{ title: "Selected", date: userSelection.date }]
+                        : []
+                    }
+                    onDayClick={(val) => calendarSelectionHandler("date", val)}
+                    onEventClick={(val) => calendarSelectionHandler("date", val)}
+                  />
+                </Grid>
+                <Grid item lg={3} md={3} sm={3} xs={3}>
+                  <Typography
+                    variant="h4"
+                    component="h1"
+                    color="textPrimary"
+                    className={classes.currentDate}
+                  >
+                    {userSelection.date
+                      ? moment(userSelection.date).format("dddd, DD")
+                      : moment().format("dddd, DD")}
+                    {userSelection.time}
+                  </Typography>
+                  {
+                    timings.map((timing) => (
+                      <Button
+                        onClick={() => calendarSelectionHandler("time", timing.value)}
+                        className={classes.timingBox}
+                        variant={userSelection.time === timing.value ? "contained" : "outlined"}
+                        color="primary"
+                        fullWidth
+                      >
+                        {timing.label}
+                      </Button>
+                    ))
+                  }
+                </Grid>
+              </Grid>
+            </Grid>
+          </>
+        )}
     </div>
   );
-}
+};
+
+export default Appointments;
---
+++ client/src/screens/patient-portal/Appointments/Calendar/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+// eslint-disable-next-line import/order
+import FullCalendar from "@fullcalendar/react"; // this import should be at the top
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import timeGridPlugin from "@fullcalendar/timegrid";
+import PropTypes from "prop-types";
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <p
+        style={{
+          color: "#fff",
+          backgroundColor: eventInfo.event.backgroundColor,
+          width: "100%",
+          padding: "3px 5px",
+          borderRadius: "3px",
+          cursor: "pointer",
+        }}
+      >
+        {eventInfo.timeText}
+        {" "}
+        {eventInfo.event.title}
+      </p>
+    </>
+  );
+}
+
+const EventCalendar = ({ events, onDayClick, onEventClick }) => (
+  <FullCalendar
+    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+    headerToolbar={{
+      left: "title",
+      center: "",
+      right: "prev next",
+    }}
+    initialView="dayGridMonth"
+    weekends
+    events={events}
+    eventContent={renderEventContent}
+    dateClick={(arg) => onDayClick(arg.dateStr)}
+    eventClick={(info) => onEventClick(info)}
+  />
+);
+
+EventCalendar.propTypes = {
+  onDayClick: PropTypes.func.isRequired,
+  onEventClick: PropTypes.func.isRequired,
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      start_dt: PropTypes.string.isRequired,
+      end_dt: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default EventCalendar;
