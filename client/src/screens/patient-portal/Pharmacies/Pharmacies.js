be1facfb21d5f2602cb7a7513035d1fbec780b3e
jve0902 <jay090218@gmail.com>
2020-11-23 15:59:08 +0500

    Pharmacies UI

--- client/src/screens/patient-portal/Pharmacies/Pharmacies.js
+++ client/src/screens/patient-portal/Pharmacies/Pharmacies.js
@@ -1,19 +1,36 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
+import {
+  makeStyles, TextField, Grid, Typography,
+} from "@material-ui/core";
+
+import {
+  Pharmacies as pharmacies,
+} from "../../../static/patientBasicInfoForm";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: "40px 0px",
   },
+  cardRoot: {
+    border: "1px solid",
+    margin: theme.spacing(0, 0, 1, 0),
+    borderRadius: 0,
+  },
   title: {
     paddingBottom: theme.spacing(1),
   },
+  inputTextRow: {
+    marginBottom: theme.spacing(3),
+  },
+  halfSectionCard: {
+    padding: theme.spacing(1.5, 0, 1, 0),
+    minHeight: 198,
+  },
 }));
 
-export default function Pharmacies() {
+const Pharmacies = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -25,6 +42,27 @@ export default function Pharmacies() {
       >
         Pharmacies
       </Typography>
+      <Grid item xs={6}>
+        <Grid className={classes.halfSectionCard}>
+          <Grid container>
+            {pharmacies.map((pharmacy, index) => (
+              <Grid key={pharmacy.name} item md={4}>
+                <Typography variant="h4" color="textPrimary">
+                  Pharmacy #
+                  {" "}
+                  {index + 1}
+                </Typography>
+                <TextField label={pharmacy.name} className={classes.inputTextRow} />
+                <Typography gutterBottom>{pharmacy.name}</Typography>
+                <Typography gutterBottom>{pharmacy.address}</Typography>
+                <Typography gutterBottom>{pharmacy.phone}</Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
+
+export default Pharmacies;
