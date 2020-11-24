e029ffda8c45679fd81a9fda99e6ba797b3f40be
jve0902 <jay090218@gmail.com>
2020-11-24 15:59:38 +0500

    Requisitions basic UI

--- client/src/screens/patient-portal/Requisition/Requisition.js
+++ client/src/screens/patient-portal/Requisition/Requisition.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
+import {
+  Button, makeStyles, Typography, Grid, TextField, MenuItem,
+} from "@material-ui/core";
+
+import { timings } from "../../../static/patient-portal/appointments";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,10 +14,22 @@ const useStyles = makeStyles((theme) => ({
   title: {
     paddingBottom: theme.spacing(1),
   },
+  btnContainer: {
+    margin: theme.spacing(1, 0),
+  },
+  btn: {
+    marginRight: 15,
+    minWidth: 120,
+  },
 }));
 
-export default function Encounters() {
+const Encounters = () => {
   const classes = useStyles();
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={classes.root}>
       <Typography
@@ -25,6 +40,43 @@ export default function Encounters() {
       >
         Requisition
       </Typography>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+        <form onSubmit={onFormSubmit}>
+          <TextField
+            select
+            required
+            variant="outlined"
+            label="Lab Requisitions"
+            margin="dense"
+            fullWidth
+          >
+            {timings.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </form>
+      </Grid>
+      <Grid
+        container
+        className={classes.btnContainer}
+      >
+        <Button
+          variant="outlined"
+          className={classes.btn}
+        >
+          Download
+        </Button>
+        <Button
+          variant="outlined"
+          className={classes.btn}
+        >
+          Print
+        </Button>
+      </Grid>
     </div>
   );
-}
+};
+
+export default Encounters;
