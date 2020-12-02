847868e6836557b05eb7ba8b722ac77afad57728
jve0902 <jay090218@gmail.com>
2020-12-02 12:52:10 +0500

    createBilling api in billings.controller

--- server/app/controllers/patient/billings.controller.js
+++ server/app/controllers/patient/billings.controller.js
@@ -37,8 +37,43 @@ const getBillings = async (req, res) => {
   }
 };
 
+const createBilling = async (req, res) => {
+  const { patient_id } = req.params;
+  const { dt, type_id, amount, note } = req.body.data;
+  let { payment_type } = req.body.data;
+
+  const db = makeDb(configuration, res);
+
+  if (!payment_type) {
+    payment_type = null;
+  } else {
+    payment_type = `'${payment_type}'`;
+  }
+  try {
+    const insertResponse = await db.query(
+      `insert into tran (patient_id, user_id, client_id, dt, type_id, amount, payment_type, note, created, created_user_id) values 
+        (${req.client_id}, ${req.user_id}, ${req.client_id}, '${dt}', ${type_id}, ${amount}, ${payment_type}, '${note}', now(), ${req.user_id})`
+    );
+
+    if (!insertResponse.affectedRows) {
+      errorMessage.error = "Insert not successful";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = insertResponse;
+    successMessage.message = "Insert successful";
+    return res.status(status.created).send(successMessage);
+  } catch (err) {
+    console.log("err", err);
+    errorMessage.error = "Insert not successful";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
 const Billing = {
   getBillings,
+  createBilling
 };
 
 module.exports = Billing;
