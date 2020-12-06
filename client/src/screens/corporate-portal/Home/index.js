fb1fef53ae232d346b379d4959101c3025df57f1
jve0902 <jay090218@gmail.com>
2020-12-06 08:44:11 +0100

    add case dropdown on header

--- client/src/screens/corporate-portal/Home/index.js
+++ client/src/screens/corporate-portal/Home/index.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
@@ -8,23 +12,61 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: "40px 0px",
   },
+  titleWrap: {
+    display: "flex",
+    width: "400px",
+    alignItems: "baseline",
+    justifyContent: "space-between",
+  },
   title: {
     paddingBottom: theme.spacing(1),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 160,
   },
 }));
 
 export default function Home() {
   const classes = useStyles();
+  const [caseStatus, setCaseStatus] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (event) => {
+    setCaseStatus(event.target.value);
+  };
+
   return (
     <div className={classes.root}>
-      <Typography
-        component="h1"
-        variant="h2"
-        color="textPrimary"
-        className={classes.title}
-      >
-        Corporate home
-      </Typography>
+      <div className={classes.titleWrap}>
+        <Typography
+          component="h1"
+          variant="h2"
+          color="textPrimary"
+          className={classes.title}
+        >
+          Home
+        </Typography>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-controlled-open-select-label">View Open Cases</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={open}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            value={caseStatus}
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="DC">DC</MenuItem>
+            <MenuItem value="WC">WC</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
     </div>
   );
 }
