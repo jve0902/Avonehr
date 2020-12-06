2b47735ad3c3a08a89c7f7ddfab9397497236b28
jve0902 <jay090218@gmail.com>
2020-12-06 13:11:22 +0500

    redirect route updated for patient-portal

--- client/src/components/AuthGuard.js
+++ client/src/components/AuthGuard.js
@@ -9,6 +9,9 @@ const AuthGuard = ({ children }) => {
   const { isAuthenticated, user } = useAuth();
 
   if (!isAuthenticated) {
+    if (user.role === "PATIENT") {
+      return <Redirect to={(user && user.login_url) || "/login/ultrawellness"} />;
+    }
     return <Redirect to={(user && user.login_url) || "/login_client"} />;
   }
 
