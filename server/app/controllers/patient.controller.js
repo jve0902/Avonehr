c0dcbcd36773358d01e354419300c08d090ad7cf
jve0902 <jay090218@gmail.com>
2020-12-16 17:22:53 +0100

    Disable prefer-arrow-callback on patient controller

--- server/app/controllers/patient.controller.js
+++ server/app/controllers/patient.controller.js
@@ -9,6 +9,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log("req:", req.body);
     const dest = process.env.LAB_UPLOAD_DIR;
+    // eslint-disable-next-line prefer-arrow-callback
     fs.access(dest, function (error) {
       if (error) {
         console.log("Directory does not exist.");
