a4ccdf10b8101e2122e18fbda2cdec36042a4c73
jve0902 <jay090218@gmail.com>
2020-12-18 08:24:41 +0100

    Refactor Header and Footer for MainLayout

---
+++ client/src/layouts/MainLayout/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    clear: "both",
+    position: "relative",
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+    },
+  },
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <Container component="footer" maxWidth={false} className={classes.footer}>
+      <Box mt={5}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Copyright © "}
+          {" "}
+          {new Date().getFullYear()}
+          {" Clinios"}
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
