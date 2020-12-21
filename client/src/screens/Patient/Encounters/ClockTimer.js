b49b09e0a5c72b11a14789b5eb0f2e82f44c0e36
jve0902 <jay090218@gmail.com>
2020-12-21 22:26:24 +0500

    timer component for encounters

---
+++ client/src/screens/Patient/Encounters/ClockTimer.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import { Box, Typography, Button } from "@material-ui/core";
+
+const Clock = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isTimerOn, setIsTimerOn] = useState(false);
+  const interval = useRef(null);
+
+  useEffect(() => () => {
+    clearInterval(interval.current);
+  }, []);
+
+  const start = () => {
+    setIsTimerOn(true);
+    interval.current = setInterval(() => {
+      setSeconds((prevState) => prevState + 1);
+    }, 1000);
+  };
+
+  const reset = () => {
+    setSeconds(0);
+    clearInterval(interval.current);
+  };
+
+  const pause = () => {
+    setIsTimerOn(false);
+    clearInterval(interval.current);
+  };
+
+  const restart = () => {
+    reset();
+    start();
+  };
+
+  const pad = (val) => (val > 9 ? val : `0${val}`);
+
+  const minutes = pad(Math.floor((seconds / 60) % 60));
+  const hours = pad(Math.floor(minutes / 60));
+
+  return (
+    <>
+      <Box pr={2} pl={2}>
+        <Typography variant="body1">
+          {hours}
+          {" "}
+          :
+          {minutes}
+          {" "}
+          :
+          {pad(seconds % 60)}
+        </Typography>
+      </Box>
+      {
+        isTimerOn
+          ? <Button variant="text" onClick={() => pause()}>Pause</Button>
+          : <Button variant="text" onClick={() => start()}>Start</Button>
+      }
+      <Button variant="text" onClick={() => reset()}>Reset</Button>
+      <Button variant="text" onClick={() => restart()}>Restart</Button>
+    </>
+  );
+};
+
+export default Clock;
