198e0b57333c5912e14c6861f7b0f0069c986239
jve0902 <jay090218@gmail.com>
2020-12-22 18:49:58 +0100

    Add end points for labs

--- server/app/routes/process-lab.routes.js
+++ server/app/routes/process-lab.routes.js
@@ -4,6 +4,7 @@ const ProcessLab = require("../controllers/process-lab.controller.js");
 
 const router = express.Router();
 
-router.get("/process-lab", [authJwt.verifyToken], ProcessLab.getAll);
+router.get("/lab/:userId/:labId", [authJwt.verifyToken], ProcessLab.getLabById);
+router.get("/lab/:userId", [authJwt.verifyToken], ProcessLab.getAll);
 
 module.exports = router;
