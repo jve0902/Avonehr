470731a79949b18b7513456ce76b7e489c4259b0
jve0902 <jay090218@gmail.com>
2020-12-28 18:03:52 +0100

    Add lab history end point

--- server/app/controllers/process-lab.controller.js
+++ server/app/controllers/process-lab.controller.js
@@ -60,9 +60,49 @@ const getAll = async (req, res) => {
   }
 };
 
+const getLabHistory = async (req, res) => {
+  const db = makeDb(configuration, res);
+  const { labId } = req.params;
+
+  try {
+    const dbResponse = await db.query(
+      `select lh.created
+      , concat(u.firstname, ' ', u.lastname) created_by
+      , lh.status
+      , lh.type
+      , concat(u2.firstname, ' ', u2.lastname) assigned_to
+      , lh.note_assign assignment_note
+      , concat(p.firstname, ' ', p.lastname) patient
+      , lh.note
+      from lab_history lh
+      left join user u on u.id=lh.created_user_id
+      left join user u2 on u2.id=lh.user_id
+      left join patient p on p.id=lh.patient_id
+      where lh.client_id=${req.client_id}
+      and lh.id=${labId}
+      order by lh.created desc
+      limit 50`
+    );
+
+    if (!dbResponse) {
+      errorMessage.message = "None found";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = dbResponse;
+    return res.status(status.created).send(successMessage);
+  } catch (err) {
+    console.log("err", err);
+    errorMessage.message = "Select not successful";
+    return res.status(status.error).send(errorMessage);
+  } finally {
+    await db.close();
+  }
+};
+
 const processLab = {
   getAll,
   getLabById,
+  getLabHistory
 };
 
 module.exports = processLab;
--- server/app/routes/process-lab.routes.js
+++ server/app/routes/process-lab.routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/lab/:userId/:labId", [authJwt.verifyToken], ProcessLab.getLabById);
 router.get("/lab/:userId", [authJwt.verifyToken], ProcessLab.getAll);
+router.get("/lab/histroy/:labId", [authJwt.verifyToken], ProcessLab.getLabHistory);
 
 module.exports = router;
