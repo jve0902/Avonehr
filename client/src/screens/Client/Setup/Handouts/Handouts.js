870c63a9c5fc044cfd02b44e7d98f3f8d0cd562f
jve0902 <jay090218@gmail.com>
2020-12-29 10:15:42 +0100

    Work on handouts UI

--- client/src/screens/Client/Setup/Handouts/Handouts.js
+++ client/src/screens/Client/Setup/Handouts/Handouts.js
@@ -1,15 +1,23 @@
 import * as React from "react";
 
 import { makeStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import Video from "../../../../components/videos/Video";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: "25px 0px",
+  },
+  titleButtonWrap: {
+    display: "flex",
+    marginBottom: theme.spacing(2),
+    "& button": {
+      marginLeft: theme.spacing(4),
+    },
   },
 }));
 
@@ -19,14 +27,18 @@ const Handouts = () => {
     <div className={classes.root}>
       <Grid container justify="center" spacing={8}>
         <Grid item md={6} xs={12}>
-          <Typography
-            component="h1"
-            variant="h2"
-            color="textPrimary"
-            className={classes.title}
-          >
-            Handouts
-          </Typography>
+          <div className={classes.titleButtonWrap}>
+            <Typography
+              component="h1"
+              variant="h2"
+              color="textPrimary"
+              className={classes.title}
+            >
+              Handouts
+            </Typography>
+            <Button variant="outlined">Add</Button>
+          </div>
+          <p>These are files we give to patients about specific topics.</p>
         </Grid>
         <Grid item md={6} xs={12}>
           <Video url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
