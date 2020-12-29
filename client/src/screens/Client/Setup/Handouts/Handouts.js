8bd5c97e33cf55d45a5e2f86f0b3fabe26adce1a
jve0902 <jay090218@gmail.com>
2020-12-29 18:00:36 +0100

    Add handouts table

--- client/src/screens/Client/Setup/Handouts/Handouts.js
+++ client/src/screens/Client/Setup/Handouts/Handouts.js
@@ -3,7 +3,15 @@ import * as React from "react";
 import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
+
 
 import Video from "../../../../components/videos/Video";
 
@@ -12,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: "25px 0px",
   },
+  table: {
+    minWidth: 650,
+  },
   titleButtonWrap: {
     display: "flex",
     marginBottom: theme.spacing(2),
@@ -19,7 +30,24 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: theme.spacing(4),
     },
   },
+  contentWrap: {
+    display: "flex",
+  },
 }));
+
+function createData(name, calories, fat, carbs, protein) {
+  return {
+    name, calories, fat, carbs, protein,
+  };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 const Handouts = () => {
   const classes = useStyles();
@@ -39,6 +67,30 @@ const Handouts = () => {
             <Button variant="outlined">Add</Button>
           </div>
           <p>These are files we give to patients about specific topics.</p>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Filename</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                  <TableCell align="right">Created</TableCell>
+                  <TableCell align="right">CreatedBy</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
         <Grid item md={6} xs={12}>
           <Video url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
