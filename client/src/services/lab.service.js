9d8434256f3efc961bb7bada65460571789312f3
jve0902 <jay090218@gmail.com>
2020-12-31 13:29:58 +0500

    Service added for lab page

---
+++ client/src/services/lab.service.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+import { API_BASE } from "../utils/API_BASE";
+import authHeader from "./auth-header";
+
+class LabService {
+  // Lab by Id
+  getLabById(labId) {
+    return axios
+      .get(`${API_BASE}/lab/${labId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => res.data);
+  }
+
+  // User History
+  getUserHistory(userId) {
+    return axios
+      .get(`${API_BASE}/lab/user-history/${userId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => res.data);
+  }
+
+  // Lab History
+  getLabHistory(labId) {
+    return axios
+      .get(`${API_BASE}/lab/history/${labId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => res.data);
+  }
+
+  // Lab Values
+  getLabValues(labId) {
+    return axios
+      .get(`${API_BASE}/lab/values/${labId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => res.data);
+  }
+}
+
+export default new LabService();
