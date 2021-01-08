7dea37a46428ec4d9baaf040da21c9c59997d472
jve0902 <jay090218@gmail.com>
2021-01-08 10:41:04 +0100

    Remove greenSwitch from Drugs

--- client/src/theme/overrides/MuiSwitch.js
+++ client/src/theme/overrides/MuiSwitch.js
@@ -1,9 +1,7 @@
 import { green } from "@material-ui/core/colors";
 
 export default {
-
   colorPrimary: {
-    color: green[200],
     "&$checked + $track": {
       backgroundColor: green[600],
     },
