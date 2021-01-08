362adc75b795ad73eb3b71788b9c9928a3fbd88e
jve0902 <jay090218@gmail.com>
2021-01-09 00:49:42 +0500

    created fax lab component

---
+++ client/src/screens/Patient/Encounters/components/FaxLab/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import {
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  button: {
+    minWidth: 100,
+  },
+}));
+
+const FaxLab = () => {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item lg={6}>
+        <Grid item lg={10}>
+          <TextField
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            label="Fax To"
+          />
+        </Grid>
+      </Grid>
+      <Grid item lg={6}>
+        <Grid container justify="space-between">
+          <Button
+            className={classes.button}
+            variant="outlined"
+          >
+            Send Fax
+          </Button>
+          <Button
+            className={classes.button}
+            variant="outlined"
+          >
+            Download
+          </Button>
+          <Button
+            className={classes.button}
+            variant="outlined"
+          >
+            Print
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default FaxLab;
