4e6bd61b38168c9d1b21f43ec492ea035c5fe26d
jve0902 <jay090218@gmail.com>
2021-01-13 13:34:02 +0500

    updated as per PR comments

--- server/app/controllers/patient.controller.js
+++ server/app/controllers/patient.controller.js
@@ -1165,14 +1165,13 @@ const getEncountersPrescriptions = async (req, res) => {
       limit 20`
     );
     if (!dbResponse) {
-      errorMessage.error = "None found";
+      errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
 
     successMessage.data = dbResponse;
     return res.status(status.created).send(successMessage);
   } catch (err) {
-    console.log("err", err);
     errorMessage.error = "Select not successful";
     return res.status(status.error).send(errorMessage);
   } finally {
