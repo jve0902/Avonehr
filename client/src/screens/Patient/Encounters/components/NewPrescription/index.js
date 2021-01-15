fe6f7a69bb43bcee4454469b31f08017b4613990
jve0902 <jay090218@gmail.com>
2021-01-15 19:22:10 +0500

    debounce drug search and frequencies drop down options

--- client/src/screens/Patient/Encounters/components/NewPrescription/index.js
+++ client/src/screens/Patient/Encounters/components/NewPrescription/index.js
@@ -14,19 +14,42 @@ import {
   Radio,
   FormControl,
   FormControlLabel,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  MenuItem,
 } from "@material-ui/core";
-import {
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
+import { makeStyles } from "@material-ui/core/styles";
+import { KeyboardDatePicker } from "@material-ui/pickers";
 import PropTypes from "prop-types";
 
 import { StyledTableCellSm, StyledTableRowSm } from "../../../../../components/common/StyledTable";
+import useDebounce from "../../../../../hooks/useDebounce";
 import PatientService from "../../../../../services/patient.service";
 import { NewDrugFormFields, GenericOptions } from "../../../../../static/encountersForm";
 
+const useStyles = makeStyles((theme) => ({
+  relativePosition: {
+    position: "relative",
+  },
+  resultsContainer: {
+    position: "absolute",
+    top: 50,
+    zIndex: 2,
+    width: "100%",
+    background: theme.palette.common.white,
+    maxHeight: 150,
+    overflow: "scroll",
+  },
+}));
+
 const NewPrescription = (props) => {
   const { onClose } = props;
+  const classes = useStyles();
   const currentDate = new Date();
+  const [drugSearchResults, setDrugSearchResults] = useState([]);
+  const [drugFrequencies, setDrugFrequencies] = useState([]);
   const [recentSelections, setRecentSelections] = useState([]);
   const [formFields, setFormFields] = useState({
     type: "",
@@ -40,6 +63,22 @@ const NewPrescription = (props) => {
     generic: "yes",
   });
 
+  const debouncedSearchTerm = useDebounce(formFields.type, 500);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      const reqBody = {
+        data: {
+          text: debouncedSearchTerm,
+        },
+      };
+      PatientService.searchEncountersPrescriptionsDrugs(reqBody)
+        .then((res) => {
+          setDrugSearchResults(res.data);
+        });
+    }
+  }, [debouncedSearchTerm]);
+
   const fetchRecentPrescriptions = useCallback(() => {
     PatientService.getEncountersPrescriptions()
       .then((response) => {
@@ -47,9 +86,17 @@ const NewPrescription = (props) => {
       });
   }, []);
 
+  const fetchDrugFrequencies = useCallback(() => {
+    PatientService.getEncountersPrescriptionsDrugsFrequencies()
+      .then((response) => {
+        setDrugFrequencies(response.data);
+      });
+  }, []);
+
   useEffect(() => {
     fetchRecentPrescriptions();
-  }, [fetchRecentPrescriptions]);
+    fetchDrugFrequencies();
+  }, [fetchRecentPrescriptions, fetchDrugFrequencies]);
 
   const handleInputChange = (e) => {
     const { value, name } = e.target;
@@ -67,6 +114,30 @@ const NewPrescription = (props) => {
     });
   };
 
+  const handleDrugTypeChange = (value) => {
+    const name = "type";
+    setFormFields({
+      ...formFields,
+      [name]: `${value} `,
+    });
+    setDrugSearchResults([]);
+  };
+
+  const renderOptionsForDropdowns = (value) => {
+    switch (value) {
+      case "Frequency": {
+        const frequencyOptions = drugFrequencies.map((option) => (
+          <MenuItem key={option.id} value={option.id}>
+            {option.descr}
+          </MenuItem>
+        ));
+        return frequencyOptions;
+      }
+      default:
+        return <div />;
+    }
+  };
+
   return (
     <>
       <Typography variant="h3" gutterBottom>
@@ -75,7 +146,13 @@ const NewPrescription = (props) => {
       <Grid container>
         <Grid item lg={4} md={4} xs={12}>
           <form>
-            <Grid item lg={10} md={10} xs={12}>
+            <Grid
+              item
+              lg={10}
+              md={10}
+              xs={12}
+              className={classes.relativePosition}
+            >
               {NewDrugFormFields.map((item) => (
                 <Box mb={1} key={item.id}>
                   {item.type === "date"
@@ -107,10 +184,34 @@ const NewPrescription = (props) => {
                         fullWidth
                         onChange={(e) => handleInputChange(e)}
                         required
-                      />
+                        select={item.baseType === "select"}
+                        inputProps={{
+                          autoComplete: "off",
+                        }}
+                      >
+                        {item.baseType === "select" && renderOptionsForDropdowns(item.label)}
+                      </TextField>
                     )}
                 </Box>
               ))}
+              {
+                (!!drugSearchResults && drugSearchResults.length) ? (
+                  <Paper className={classes.resultsContainer}>
+                    <List>
+                      {drugSearchResults.map((drug) => (
+                        <ListItem
+                          button
+                          onClick={() => handleDrugTypeChange(drug.name)}
+                          key={drug.name}
+                        >
+                          <ListItemText primary={drug.name} />
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Paper>
+                )
+                  : null
+              }
               <FormControl component="fieldset">
                 <RadioGroup
                   row
