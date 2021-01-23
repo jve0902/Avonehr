5001bb36a7303b6d966e87efd7067aaf6d520598
jve0902 <jay090218@gmail.com>
2021-01-23 12:44:31 +0500

    Added home button on 404

--- client/src/screens/errors/NotFound.js
+++ client/src/screens/errors/NotFound.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,6 +35,25 @@ const useStyles = makeStyles((theme) => ({
     color: "#b0bec5",
     margin: "0",
   },
+  button: {
+    color: theme.palette.common.black,
+    letterSpacing: "0.02857em",
+    backgroundColor: "#b0bec5",
+    width: "auto",
+    height: "40px",
+    padding: "0 16px",
+    minWidth: "50px",
+    minHeight: "auto",
+    borderRadius: "24px",
+    marginTop: 25,
+
+    "&:hover": {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  link: {
+    textDecoration: "none !important",
+  },
 }));
 
 const NotFound = () => {
@@ -42,6 +63,9 @@ const NotFound = () => {
       <div className={classes.bg} />
       <h1 className={classes.title}>404</h1>
       <p className={classes.subTitle}>Not Found!</p>
+      <Link className={classes.link} to="/">
+        <Button className={classes.button}>Go to Home</Button>
+      </Link>
     </div>
   );
 };
