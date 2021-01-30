4109872c04f1657ee738169a60660765e90f68d8
jve0902 <jay090218@gmail.com>
2021-01-30 15:40:57 +0500

    Integrated billing card component

--- client/src/screens/Patient/Encounters/index.js
+++ client/src/screens/Patient/Encounters/index.js
@@ -41,6 +41,7 @@ import MedicationsCardContent from "../Medications/content";
 import MessagesCardContent from "../Messages/content";
 import RequisitionsCardContent from "../Requisitions/content";
 import TestsCardContent from "../Tests/content";
+import BillingCard from "./components/BillingCard";
 import Card from "./components/Card";
 import ClockTimer from "./components/ClockTimer";
 import PlanCard from "./components/PlanCard";
@@ -370,7 +371,7 @@ const Encounters = (props) => {
                   />
                   <Card
                     title="Billing"
-                    data=""
+                    data={<BillingCard />}
                     icon
                   />
 
