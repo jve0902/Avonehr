5e83147bc776439844894addddf4c0c8b0ac72dc
jve0902 <jay090218@gmail.com>
2021-02-03 17:46:54 +0500

    Moved billing payments dialog component to billing/components folder

--- client/src/screens/Patient/Encounters/components/Billng/components/BillingPayment/index.js
+++ client/src/screens/Patient/Encounters/components/Billng/components/BillingPayment/index.js
@@ -6,7 +6,7 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
-import Dialog from "../../../../../components/Dialog";
+import Dialog from "../../../../../../../components/Dialog";
 
 const useStyles = makeStyles(() => ({
   minWidth100: {
