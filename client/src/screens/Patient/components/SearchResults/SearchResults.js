578d0f0860394bd76d429eeaae82b759f9397dbd
jve0902 <jay090218@gmail.com>
2021-02-08 19:17:24 +0500

    Added search results component

---
+++ client/src/screens/Patient/components/SearchResults/SearchResults.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { Typography, Divider, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+  ml1: {
+    marginLeft: theme.spacing(1),
+  },
+  label: {
+    fontWeight: 600,
+  },
+  text: {
+    fontSize: 14,
+  },
+}));
+
+const SearchResults = (props) => {
+  const { data } = props;
+  const classes = useStyles();
+
+  return (
+    data.map((item, index) => (
+      <>
+        <Typography variant="h5" gutterBottom className={classes.label}>{item.Encounter}</Typography>
+        <Typography variant="body1" gutterBottom>
+          <span className={classes.label}>
+            Encounter:
+          </span>
+          <span className={`${classes.ml1} ${classes.text}`}>
+            {item.Encounter}
+          </span>
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <span className={classes.label}>
+            Date:
+          </span>
+          <span className={`${classes.ml1} ${classes.text}`}>
+            {item.dt}
+          </span>
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <span className={classes.label}>
+            Notes:
+          </span>
+          <span className={`${classes.ml1} ${classes.text}`}>
+            {item.notes}
+          </span>
+        </Typography>
+        {
+          index + 1 !== data.length
+            ? <Divider className={classes.divider} />
+            : null
+        }
+      </>
+    ))
+  );
+};
+
+export default SearchResults;
