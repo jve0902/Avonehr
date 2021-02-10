d89b06caa711ea07060c401accf74977c9e61be4
jve0902 <jay090218@gmail.com>
2021-02-10 18:59:36 +0500

    Created Payment method form

---
+++ client/src/screens/Patient/components/BasicInfo/components/PaymentMethodsForm/index.js
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from "react";
+
+import {
+  TextField, Button, Grid, Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
+import { useSnackbar } from "notistack";
+import PropTypes from "prop-types";
+
+import MaskInput from "../../../../../../components/common/MaskInput";
+import Dialog from "../../../../../../components/Dialog";
+import useDidMountEffect from "../../../../../../hooks/useDidMountEffect";
+import PatientPortalService from "../../../../../../services/patient_portal/patient-portal.service";
+
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    margin: theme.spacing(3, 0),
+  },
+  gutterBottom: {
+    marginBottom: theme.spacing(1),
+  },
+  customLabel: {
+    fontSize: 16,
+    color: "#37474f",
+    marginBottom: theme.spacing(2),
+  },
+  buttonsContainer: {
+    margin: theme.spacing(3, 0),
+  },
+}));
+
+const PaymentMethodsForm = (props) => {
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const {
+    isOpen, onClose, reloadData, cardData,
+  } = props;
+  const isEdit = Boolean(cardData);
+
+  const [formFields, setFormFields] = useState({
+    cardType: "",
+    cardNumber: "",
+    cvv: "",
+    expiryDate: "",
+  });
+
+  useEffect(() => {
+    if (cardData) {
+      setFormFields({ ...cardData });
+    }
+  }, [cardData]);
+
+  const updateFormState = (key, value) => {
+    setFormFields({
+      ...formFields,
+      [key]: value,
+    });
+  };
+
+  const handleInputChnage = (e) => {
+    const { value, name } = e.target;
+    updateFormState(name, value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const reqBody = {
+      data: {
+        dt: moment(formFields.date).format("YYYY-MM-DD hh:mm"),
+        type_id: formFields.type,
+        payment_type: formFields.paymentType,
+        amount: formFields.amount,
+        note: formFields.notes,
+        account_number: formFields.accountNum,
+      },
+    };
+    PatientPortalService.createBilling(reqBody).then((response) => {
+      enqueueSnackbar(`${response.message}`, { variant: "success" });
+      reloadData();
+      onClose();
+    });
+  };
+
+  const getCardType = () => {
+    const { cardNumber } = formFields;
+    const cardFirstDigit = cardNumber[0];
+    const cardType = "cardType";
+
+    if (cardFirstDigit === "3") {
+      updateFormState(cardType, "American Express");
+    } else if (cardFirstDigit === "4") {
+      updateFormState(cardType, "Visa");
+    } else if (cardFirstDigit === "5") {
+      updateFormState(cardType, "Master");
+    } else {
+      updateFormState(cardType, "");
+    }
+  };
+
+  useDidMountEffect(() => {
+    getCardType();
+  }, [formFields.cardNumber]);
+
+  return (
+    <Dialog
+      open={isOpen}
+      title={" "}
+      message={(
+        <>
+          <Typography variant="h3" color="textSecondary">
+            {isEdit ? "Edit" : "Add"}
+            {" "}
+            Payment Method
+          </Typography>
+          <form onSubmit={onFormSubmit}>
+            <Grid className={classes.formContainer}>
+              <Grid>
+                <MaskInput
+                  required
+                  className={classes.gutterBottom}
+                  type="text"
+                  name="cardNumber"
+                  label="Card Number"
+                  margin="dense"
+                  variant="outlined"
+                  value={formFields.cardNumber}
+                  mask="9999/9999/9999/9999"
+                  onChange={(e) => handleInputChnage(e)}
+                />
+                {!!formFields.cardType && formFields.cardType.length ? (
+                  <Typography gutterBottom>{formFields.cardType}</Typography>
+                )
+                  : null}
+              </Grid>
+
+              <Grid>
+                <TextField
+                  required
+                  variant="outlined"
+                  margin="dense"
+                  name="cvv"
+                  id="cvv"
+                  type="number"
+                  label="CVV"
+                  className={classes.gutterBottom}
+                  value={formFields.cvv}
+                  onChange={(e) => handleInputChnage(e)}
+                  onInput={(e) => {
+                    e.target.value = Math.max(0, parseInt(e.target.value, 10)).toString().slice(0, 3);
+                  }}
+                />
+              </Grid>
+
+              <Grid>
+                <MaskInput
+                  required
+                  className={classes.gutterBottom}
+                  type="text"
+                  name="expiryDate"
+                  label="Validity"
+                  margin="dense"
+                  variant="outlined"
+                  value={formFields.expiryDate}
+                  mask="99/99"
+                  onChange={(e) => handleInputChnage(e)}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container className={classes.buttonsContainer} justify="space-between">
+              <Button variant="outlined" type="submit">
+                Add Method
+              </Button>
+              <Button variant="outlined" onClick={() => onClose()}>
+                Cancel
+              </Button>
+            </Grid>
+          </form>
+        </>
+      )}
+      cancelForm={() => onClose()}
+      hideActions
+      size="xs"
+    />
+  );
+};
+
+PaymentMethodsForm.defaultProps = {
+  cardData: null,
+};
+
+PaymentMethodsForm.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  reloadData: PropTypes.func.isRequired,
+  cardData: PropTypes.shape({}),
+};
+
+export default PaymentMethodsForm;
