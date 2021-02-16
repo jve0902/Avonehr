f8526503b2cc648a69d6bc3c51ebdcd79de975fb
jve0902 <jay090218@gmail.com>
2021-02-16 10:50:23 +0500

    Added selectable payment methods in ecounters billing payment dialog

--- client/src/screens/Patient/Encounters/components/Billng/components/BillingPaymentDialog/index.js
+++ client/src/screens/Patient/Encounters/components/Billng/components/BillingPaymentDialog/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import {
-  Box, Typography, Grid, Button, TextField,
+  Box, Typography, Grid, Button, TextField, List, ListItem, ListItemText,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import moment from "moment";
@@ -11,10 +11,22 @@ import PropTypes from "prop-types";
 import Dialog from "../../../../../../../components/Dialog";
 import usePatientContext from "../../../../../../../hooks/usePatientContext";
 import PatientService from "../../../../../../../services/patient.service";
+import { paymentMethodType } from "../../../../../../../utils/helpers";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   minWidth100: {
     minWidth: 100,
+  },
+  selected: {
+    border: `2px solid ${theme.palette.primary.main}`,
+    borderRadius: 5,
+    padding: 5,
+    cursor: "pointer",
+  },
+  listItem: {
+    border: `2px solid transparent`,
+    padding: 5,
+    cursor: "pointer",
   },
 }));
 
@@ -22,28 +34,35 @@ const BillingPayment = (props) => {
   const { open, onClose, reloadData } = props;
   const classes = useStyles();
   const [amount, setAmount] = useState("");
+  const [selectedAccount, setSelectedAccount] = useState(null);
 
   const { state } = usePatientContext();
   const { enqueueSnackbar } = useSnackbar();
 
   const { patientId } = state;
   const encounterId = state.encounters.selectedEncounter?.id || 1;
+  const paymentMethodsData = state.patientInfo.paymentMethods || [];
 
   const onFormSubmit = (e) => {
     e.preventDefault();
-    const reqBody = {
-      data: {
-        dt: moment().format("YYYY-MM-DD hh:mm"),
-        type_id: 3,
-        payment_type: "C",
-        amount,
-      },
-    };
-    PatientService.createEncountersBillingPayments(patientId, encounterId, reqBody).then((response) => {
-      enqueueSnackbar(`${response.message}`, { variant: "success" });
-      reloadData();
-      onClose();
-    });
+    if (selectedAccount) {
+      const reqBody = {
+        data: {
+          account_number: selectedAccount.account_number,
+          dt: moment().format("YYYY-MM-DD hh:mm"),
+          type_id: 3,
+          payment_type: "C",
+          amount,
+        },
+      };
+      PatientService.createEncountersBillingPayments(patientId, encounterId, reqBody).then((response) => {
+        enqueueSnackbar(`${response.message}`, { variant: "success" });
+        reloadData();
+        onClose();
+      });
+    } else {
+      enqueueSnackbar(`Select account number`, { variant: "error" });
+    }
     e.stopPropagation(); // to prevent encounters main form submission
   };
 
@@ -66,9 +85,40 @@ const BillingPayment = (props) => {
             >
               Use existing payment method
             </Typography>
-            <Typography gutterBottom>Visa 0043</Typography>
+            <Grid item lg={6}>
+              <List component="ul">
+                {
+                  paymentMethodsData.length
+                    ? paymentMethodsData.map((item) => {
+                      const isItemSelected = !!selectedAccount && selectedAccount.id === item.id;
+                      return (
+                        <ListItem
+                          key={item.id}
+                          disableGutters
+                          button
+                          onClick={() => setSelectedAccount(item)}
+                          className={isItemSelected ? classes.selected : classes.listItem}
+                        >
+                          <ListItemText primary={`${paymentMethodType(item.type)} ${item.account_number}`} />
+                        </ListItem>
+                      );
+                    })
+                    : (
+                      <Typography
+                        className={classes.formInput}
+                        variant="h5"
+                        color="textPrimary"
+                        gutterBottom
+                      >
+                        No payment methods available...
+                      </Typography>
+                    )
+                }
+              </List>
+            </Grid>
+            {/* <Typography gutterBottom>Visa 0043</Typography>
             <Typography gutterBottom>Master Card 0222</Typography>
-            <Typography gutterBottom>Checking 0111</Typography>
+            <Typography gutterBottom>Checking 0111</Typography> */}
           </Box>
 
           <Box mb={3} mt={3}>
