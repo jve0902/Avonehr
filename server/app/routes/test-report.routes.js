34c24f5229a9f51467c597182a29d2490ca243ad
jve0902 <jay090218@gmail.com>
2021-03-01 12:33:29 +0100

    Get test graph

--- server/app/routes/test-report.routes.js
+++ server/app/routes/test-report.routes.js
@@ -8,6 +8,7 @@ router.get("/tests", [authJwt.verifyToken], testReports.getFunctionalRange);
 router.get("/tests/page-title/:cptId", [authJwt.verifyToken], testReports.getPageTitle);
 router.get("/tests/lab-cpt/:patientId/:labId", [authJwt.verifyToken], testReports.getLabcptByLabId);
 router.get("/tests/lab-cpt/:patientId/lab/:labId", [authJwt.verifyToken], testReports.getLabcptByLabId);
-router.get("/tests/lab-cpt/:patientId/", [authJwt.verifyToken], testReports.getLabcpt);
+router.get("/tests/lab-cpt/:patientId", [authJwt.verifyToken], testReports.getLabcpt);
+router.get("/tests/graph/:patientId", [authJwt.verifyToken], testReports.getTestGraph);
 
 module.exports = router;
