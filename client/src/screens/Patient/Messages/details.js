623f4fe73ddbe69fc1735ed200d2924f3b0a09e8
jve0902 <jay090218@gmail.com>
2021-03-02 11:31:29 +0500

    CLIN-4 implementation

--- client/src/screens/Patient/Messages/details.js
+++ client/src/screens/Patient/Messages/details.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
@@ -10,11 +10,16 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/DeleteOutline";
+import EditIcon from "@material-ui/icons/Edit";
+import ReplyIcon from "@material-ui/icons/Reply";
 import moment from "moment";
 import { useSnackbar } from "notistack";
 import PropTypes from "prop-types";
 
+import Alert from "../../../components/Alert";
+import useAuth from "../../../hooks/useAuth";
 import usePatientContext from "../../../hooks/usePatientContext";
+import { toggleMessageDialog, setSelectedMessage, setMessageType } from "../../../providers/Patient/actions";
 import PatientService from "../../../services/patient.service";
 
 const useStyles = makeStyles((theme) => ({
@@ -68,16 +73,30 @@ const MessagesDetails = (props) => {
   const { enqueueSnackbar } = useSnackbar();
   const classes = useStyles();
 
-  const { state } = usePatientContext();
+  const { user } = useAuth();
+  const { state, dispatch } = usePatientContext();
   const { data } = state.messages;
   const { patientId } = state;
-  const actionsEnable = false;
 
-  const deleteItemHandler = (selectedItem) => {
-    const messageId = selectedItem.id;
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openDeleteDialog = (item) => {
+    setSelectedItem(item);
+    setShowDeleteDialog((prevstate) => !prevstate);
+  };
+
+  const closeDeleteDialog = () => {
+    setSelectedItem(null);
+    setShowDeleteDialog((prevstate) => !prevstate);
+  };
+
+  const deleteItemHandler = (item) => {
+    const messageId = item.id;
     PatientService.deleteMessages(patientId, messageId)
       .then((response) => {
         enqueueSnackbar(`${response.data.message}`, { variant: "success" });
+        closeDeleteDialog();
         reloadData();
       })
       .catch((error) => {
@@ -90,58 +109,107 @@ const MessagesDetails = (props) => {
       });
   };
 
+  const isDeletDisabled = useCallback((item) => {
+    if (item && item.user_id_from === user.id) {
+      return false;
+    }
+    return true;
+  }, [user.id]);
+
+  const isEditDisabled = useCallback((item) => {
+    if (item && item.user_id_from === user.id) {
+      return false;
+    }
+    return true;
+  }, [user.id]);
+
+  const isReplyDisabled = useCallback((item) => {
+    if (item && item.user_id_from === null) {
+      return false;
+    }
+    return true;
+  }, []);
+
   return (
-    <TableContainer className={classes.tableContainer}>
-      <Table size="small" className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Created</StyledTableCell>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell>Subject</StyledTableCell>
-            <StyledTableCell>Message</StyledTableCell>
-            <StyledTableCell>From</StyledTableCell>
-            <StyledTableCell>To</StyledTableCell>
-            {!!actionsEnable && (
+    <>
+      <Alert
+        open={showDeleteDialog}
+        title="Confirm Delete"
+        message="Are you sure you want to delete this message?"
+        applyButtonText="Delete"
+        cancelButtonText="Cancel"
+        applyForm={() => deleteItemHandler(selectedItem)}
+        cancelForm={closeDeleteDialog}
+      />
+      <TableContainer className={classes.tableContainer}>
+        <Table size="small" className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Created</StyledTableCell>
+              <StyledTableCell>Subject</StyledTableCell>
+              <StyledTableCell>From</StyledTableCell>
+              <StyledTableCell>To</StyledTableCell>
+              <StyledTableCell>Message</StyledTableCell>
               <StyledTableCell align="center">Actions</StyledTableCell>
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {!!data && data.length
-            ? data.map((row) => (
-              <StyledTableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {moment(row.created).format("MMM D YYYY")}
-                </TableCell>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.subject}</TableCell>
-                <TableCell>{row.message}</TableCell>
-                <TableCell>{row.user_to_from || ""}</TableCell>
-                <TableCell>{row.user_to_name || ""}</TableCell>
-                {!!actionsEnable && (
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!!data && data.length
+              ? data.map((row) => (
+                <StyledTableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {moment(row.created).format("MMM D YYYY")}
+                  </TableCell>
+                  <TableCell>{row.subject}</TableCell>
+                  <TableCell>{row.user_to_from || "Patient"}</TableCell>
+                  <TableCell>{row.user_to_name || "Patient"}</TableCell>
+                  <TableCell>{row.message}</TableCell>
                   <TableCell className={classes.actions}>
                     <IconButton
+                      disabled={isDeletDisabled(row)}
                       className={classes.button}
-                      onClick={() => deleteItemHandler(row)}
+                      onClick={() => openDeleteDialog(row)}
                     >
                       <DeleteIcon fontSize="small" />
                     </IconButton>
+                    <IconButton
+                      disabled={isEditDisabled(row)}
+                      className={classes.button}
+                      onClick={() => {
+                        dispatch(setMessageType("Edit"));
+                        dispatch(setSelectedMessage(row));
+                        dispatch(toggleMessageDialog());
+                      }}
+                    >
+                      <EditIcon fontSize="small" />
+                    </IconButton>
+                    <IconButton
+                      disabled={isReplyDisabled(row)}
+                      className={classes.button}
+                      onClick={() => {
+                        dispatch(setMessageType("Reply To"));
+                        dispatch(setSelectedMessage(row));
+                        dispatch(toggleMessageDialog());
+                      }}
+                    >
+                      <ReplyIcon fontSize="small" />
+                    </IconButton>
                   </TableCell>
-                )}
-              </StyledTableRow>
-            ))
-            : (
-              <StyledTableRow>
-                <TableCell colSpan={7}>
-                  <Typography align="center" variant="body1">
-                    No Records Found...
-                  </Typography>
-                </TableCell>
-              </StyledTableRow>
-            )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                </StyledTableRow>
+              ))
+              : (
+                <StyledTableRow>
+                  <TableCell colSpan={7}>
+                    <Typography align="center" variant="body1">
+                      No Records Found...
+                    </Typography>
+                  </TableCell>
+                </StyledTableRow>
+              )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
