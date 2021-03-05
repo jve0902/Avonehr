24585a290a3bd2c08514fbcc2d49c2f4298a1c9f
jve0902 <jay090218@gmail.com>
2021-03-05 15:09:20 +0500

    removed isDialog prop for docs and tests and used fullHeight prop for dialog

--- client/src/screens/Patient/Patient.js
+++ client/src/screens/Patient/Patient.js
@@ -898,6 +898,7 @@ const Patient = () => {
           cancelForm={() => dispatch(toggleAllergyDialog())}
           hideActions
           size="md"
+          fullHeight
         />
       )}
 
@@ -930,6 +931,7 @@ const Patient = () => {
           cancelForm={() => dispatch(toggleHandoutsDialog())}
           hideActions
           size="md"
+          fullHeight
         />
       )}
 
@@ -1043,6 +1045,7 @@ const Patient = () => {
           cancelForm={() => dispatch(toggleDiagnosesDialog())}
           hideActions
           size="lg"
+          fullHeight
         />
       )}
 
@@ -1137,13 +1140,13 @@ const Patient = () => {
             <DocumentsCardContent
               reloadData={() => fetchDocuments()}
               actionsEnable
-              isDialog
             />
           )}
           applyForm={() => dispatch(toggleDocumentsExpandDialog())}
           cancelForm={() => dispatch(toggleDocumentsExpandDialog())}
           hideActions
           size="lg"
+          fullHeight
         />
       )}
 
@@ -1152,12 +1155,13 @@ const Patient = () => {
           open={tests.expandDialog}
           title="All Tests"
           message={
-            <TestsCardContent isDialog />
+            <TestsCardContent />
           }
           applyForm={() => dispatch(toggleTestsExpandDialog())}
           cancelForm={() => dispatch(toggleTestsExpandDialog())}
           hideActions
           size="lg"
+          fullHeight
         />
       )}
 
@@ -1289,7 +1293,6 @@ const Patient = () => {
                   <DocumentsCardContent
                     reloadData={() => fetchDocuments()}
                     actionsEnable
-                    isDialog={false}
                   />
                 )}
                 showActions
@@ -1304,7 +1307,7 @@ const Patient = () => {
             <Grid key="All Tests">
               <Card
                 title="All Tests"
-                data={<TestsCardContent isDialog={false} />}
+                data={<TestsCardContent />}
                 showActions
                 primaryButtonText="Expand"
                 secondaryButtonText={null}
