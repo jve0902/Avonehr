3905c9124eb1f88eb606a31781da601ed92bf8de
jve0902 <jay090218@gmail.com>
2021-03-05 15:13:20 +0500

    CLIN-6::Handouts card revamped

--- client/src/screens/Patient/components/Handouts/Handouts.js
+++ client/src/screens/Patient/components/Handouts/Handouts.js
@@ -6,14 +6,18 @@ import {
   Button,
   Grid,
   TextField,
-  List,
-  ListItem,
-  ListItemText,
+  Typography,
+  TableContainer,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSnackbar } from "notistack";
 import PropTypes from "prop-types";
 
+import { StyledTableRowSm, StyledTableCellSm } from "../../../../components/common/StyledTable";
 import useDidMountEffect from "../../../../hooks/useDidMountEffect";
 import usePatientContext from "../../../../hooks/usePatientContext";
 import { toggleHandoutsDialog } from "../../../../providers/Patient/actions";
@@ -32,6 +36,12 @@ const useStyles = makeStyles((theme) => ({
   ml2: {
     marginLeft: theme.spacing(2),
   },
+  text: {
+    lineHeight: "21px",
+  },
+  pointer: {
+    cursor: "pointer",
+  },
 }));
 
 const HandoutsForm = (props) => {
@@ -40,6 +50,7 @@ const HandoutsForm = (props) => {
   const { reloadData } = props;
   const [searchText, setSearchText] = useState("");
   const [allHandouts, setAllHandouts] = useState([]);
+  const [hasUserSearched, setHasUserSearched] = useState(false);
   const { state, dispatch } = usePatientContext();
   const { patientId } = state;
 
@@ -47,16 +58,18 @@ const HandoutsForm = (props) => {
     e.preventDefault();
     PatientService.getAllHandouts().then((res) => {
       setAllHandouts(res.data);
+      setHasUserSearched(true);
     });
   }, []);
 
   useDidMountEffect(() => {
     if (!searchText.length) {
       setAllHandouts([]);
+      setHasUserSearched(false);
     }
   }, [searchText]);
 
-  const createPatientHandoutHandler = (item) => {
+  const onFormSubmit = (item) => {
     const reqBody = {
       data: {
         handout_id: item.id,
@@ -80,7 +93,7 @@ const HandoutsForm = (props) => {
       </Grid>
       */}
 
-      <Grid container alignItems="center">
+      <Grid container alignItems="center" className={classes.mb2}>
         <form onSubmit={(e) => fetchAllHandouts(e, searchText)}>
           <TextField
             autoFocus
@@ -99,19 +112,41 @@ const HandoutsForm = (props) => {
         </form>
       </Grid>
 
-      <Grid item lg={4} className={`${classes.root} ${classes.mb2}`}>
-        <List component="ul">
-          {allHandouts.map((handout) => (
-            <ListItem
-              onClick={() => createPatientHandoutHandler(handout)}
-              key={handout.id}
-              disableGutters
-              button
-            >
-              <ListItemText primary={handout.filename} />
-            </ListItem>
-          ))}
-        </List>
+      <Grid className={`${classes.root} ${classes.mb2}`}>
+        <TableContainer>
+          <Table stickyHeader size="small">
+            <TableHead>
+              <TableRow>
+                <StyledTableCellSm>Name</StyledTableCellSm>
+                <StyledTableCellSm>ID</StyledTableCellSm>
+                <StyledTableCellSm>Favorite</StyledTableCellSm>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allHandouts.length
+                ? allHandouts.map((item) => (
+                  <StyledTableRowSm
+                    key={item.id}
+                    className={classes.pointer}
+                    onClick={() => onFormSubmit(item)}
+                  >
+                    <StyledTableCellSm>{item.filename}</StyledTableCellSm>
+                    <StyledTableCellSm>{item.id}</StyledTableCellSm>
+                    <StyledTableCellSm>{item.favorite ? "Yes" : ""}</StyledTableCellSm>
+                  </StyledTableRowSm>
+                ))
+                : hasUserSearched ? (
+                  <StyledTableRowSm>
+                    <StyledTableCellSm colSpan={4}>
+                      <Typography align="center" variant="body1" className={classes.text}>
+                        No Records found...
+                      </Typography>
+                    </StyledTableCellSm>
+                  </StyledTableRowSm>
+                ) : null}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </>
   );
