67821e988fa291ce64a24f544238cf8e7958b0e6
jve0902 <jay090218@gmail.com>
2021-03-08 14:45:06 +0500

    CLIN-50::add calculated tests values

--- client/src/screens/Patient/Tests/content.js
+++ client/src/screens/Patient/Tests/content.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -75,7 +75,73 @@ const TestsContent = () => {
   const { gender, dob } = state.patientInfo.data;
   const patientAge = Number(calculateAge(dob).split(" ")[0]);
 
+  const [tests, setTests] = useState([]);
+
   const hasValue = (value) => !((typeof value === "undefined") || (value === null));
+
+  const hasTestValue = (value) => data.filter((x) => x.name === value);
+
+  const addCalculatedTests = useCallback(() => {
+    if (!!data && data.length) {
+      const tempTestsArray = [...data];
+      const hasSodiumTest = hasTestValue("Sodium").length ? hasTestValue("Sodium")[0] : null;
+      const hasPotassiumTest = hasTestValue("Potassium").length ? hasTestValue("Potassium")[0] : null;
+      const hasGlucoseTest = hasTestValue("Glucose").length ? hasTestValue("Glucose")[0] : null;
+      const hasUreaTest = hasTestValue("Urea").length ? hasTestValue("Urea")[0] : null;
+      if (!!hasSodiumTest && !!hasPotassiumTest && !!hasGlucoseTest && !!hasUreaTest) {
+        const newNode = {
+          count: 1,
+          cpt_id: "Osmolarity",
+          lab_dt: new Date(),
+          name: "Osmolarity",
+          range_high: calculateFunctionalRange("Osmolarity", gender, patientAge).high,
+          range_low: calculateFunctionalRange("Osmolarity", gender, patientAge).low,
+          unit: "ug/dL",
+          value: (1.9 * (hasSodiumTest.value + hasPotassiumTest.value))
+            + hasGlucoseTest.value + (hasUreaTest.value * 0.5) + 5,
+        };
+        tempTestsArray.push(newNode);
+      }
+      const hasHematocritTest = hasTestValue("Hematocrit").length ? hasTestValue("Hematocrit")[0] : null;
+      const hasProteinTotalTest = hasTestValue("Protein Total").length
+        ? hasTestValue("Protein Total")[0] : null;
+      if (!!hasHematocritTest && !!hasProteinTotalTest) {
+        const newNode = {
+          count: 1,
+          cpt_id: "ViscosityHighShear",
+          lab_dt: new Date(),
+          name: "Viscosity High Shear",
+          range_high: calculateFunctionalRange("ViscosityHighShear", gender, patientAge).high,
+          range_low: calculateFunctionalRange("ViscosityHighShear", gender, patientAge).low,
+          unit: "ug/dL",
+          value: (0.12 * hasHematocritTest.value) + (0.17 * ((hasProteinTotalTest.value * 10) - 2.07)),
+        };
+        tempTestsArray.push(newNode);
+      }
+      const hasChlorideTest = hasTestValue("Chloride").length ? hasTestValue("Chloride")[0] : null;
+      const hasCarbonDioxideTest = hasTestValue("Carbon Dioxide").length
+        ? hasTestValue("Carbon Dioxide")[0] : null;
+      if (!!hasSodiumTest && !!hasChlorideTest && !!hasCarbonDioxideTest) {
+        const newNode = {
+          count: 1,
+          cpt_id: "AnionGapNaClHCO3",
+          lab_dt: new Date(),
+          name: "Anion Gap Na-(Cl+HCO3)",
+          range_high: calculateFunctionalRange("AnionGapNaClHCO3", gender, patientAge).high,
+          range_low: calculateFunctionalRange("AnionGapNaClHCO3", gender, patientAge).low,
+          unit: "ug/dL",
+          value: hasSodiumTest.value - (hasChlorideTest.value + hasCarbonDioxideTest.value),
+        };
+        tempTestsArray.push(newNode);
+      }
+      setTests([...tempTestsArray]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, gender, patientAge]);
+
+  useEffect(() => {
+    addCalculatedTests();
+  }, [addCalculatedTests]);
 
   return (
     <>
@@ -96,8 +162,8 @@ const TestsContent = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {!!data && data.length
-              ? data.map((row) => {
+            {!!tests && tests.length
+              ? tests.map((row) => {
                 const functionalRange = calculateFunctionalRange(row.cpt_id, gender, patientAge);
                 return (
                   <StyledTableRow key={row.name}>
