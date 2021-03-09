1c6a8bb6f18d6f8ead2c119e412cbc8e13b0e8b5
jve0902 <jay090218@gmail.com>
2021-03-09 12:50:04 +0500

    removed table header and col width issue in handouts

--- client/src/screens/Patient/components/Handouts/Handouts.js
+++ client/src/screens/Patient/components/Handouts/Handouts.js
@@ -10,8 +10,6 @@ import {
   TableContainer,
   Table,
   TableBody,
-  TableHead,
-  TableRow,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSnackbar } from "notistack";
@@ -123,11 +121,6 @@ const HandoutsForm = (props) => {
       <Grid className={`${classes.root} ${classes.mb2}`}>
         <TableContainer>
           <Table stickyHeader size="small">
-            <TableHead>
-              <TableRow>
-                <StyledTableCellSm>Name</StyledTableCellSm>
-              </TableRow>
-            </TableHead>
             <TableBody>
               {allHandouts.length
                 ? allHandouts.map((item) => (
