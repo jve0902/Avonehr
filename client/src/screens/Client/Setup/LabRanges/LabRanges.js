cdc6c78884639623532b4cbb71d622bd5be5cf8a
jve0902 <jay090218@gmail.com>
2021-03-11 13:31:11 +0500

    CLIN-56 UI completed

--- client/src/screens/Client/Setup/LabRanges/LabRanges.js
+++ client/src/screens/Client/Setup/LabRanges/LabRanges.js
@@ -16,13 +16,15 @@ import {
   TableCell,
 } from "@material-ui/core";
 import moment from "moment";
+import { useSnackbar } from "notistack";
 
+import Alert from "../../../../components/Alert";
 import DropDown from "../../../../components/common/DropDown";
 import { StyledTableCellLg, StyledTableRowLg } from "../../../../components/common/StyledTable";
-import useDebounce from "../../../../hooks/useDebounce";
 import LabRangeService from "../../../../services/setup/labrange.service";
 import { AgeOptions, GenderOptions } from "../../../../static/setup/labRange";
-import Counter from "./components/counter";
+import Counter from "./components/Counter";
+import NewLabRange from "./components/NewLabRange";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -59,15 +61,25 @@ const useStyles = makeStyles((theme) => ({
 
 const LabRanges = () => {
   const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const [labSearch, setLabSearch] = useState("");
-  const [labSearchResults, setLabSearchResults] = useState([]); // live search results
-  const [useFuncRange, setUseFuncRange] = useState(false);
-  const [labs, setLabs] = useState([]);
+  const [selectedRange, setSelectedRange] = useState({});
+  const [showNewRangeDialog, setShowNewRangeDialog] = useState(false);
+  const [showResetDialog, setShowResetDialog] = useState(false);
+  const [useFuncRange, setUseFuncRange] = useState(true);
+  const [labRanges, setLabRanges] = useState([]);
+
+  const openResetDialog = () => {
+    setShowResetDialog((prevstate) => !prevstate);
+  };
+
+  const closeResetDialog = () => {
+    setShowResetDialog((prevstate) => !prevstate);
+  };
 
   const fetchLabs = useCallback(() => {
     LabRangeService.getLabRanges().then((res) => {
-      setLabs(res.data);
+      setLabRanges(res.data);
     });
   }, []);
 
@@ -75,94 +87,107 @@ const LabRanges = () => {
     fetchLabs();
   }, [fetchLabs]);
 
-  const debouncedSearchTerm = useDebounce(labSearch, 500);
-
-  useEffect(() => {
-    if (debouncedSearchTerm) {
-      const reqBody = {
-        data: {
-          text: debouncedSearchTerm,
-        },
-      };
-      LabRangeService.searchLabs(reqBody).then((res) => {
-        setLabSearchResults(res.data);
-      });
-    }
-  }, [debouncedSearchTerm]);
-
   const handleChangeFuncRange = () => {
     setUseFuncRange((prevState) => !prevState);
   };
 
   const deleteItemHandler = (index) => {
-    const labsDataClone = [...labs];
+    const labsDataClone = [...labRanges];
     labsDataClone.splice(index, 1);
-    setLabs([...labsDataClone]);
+    setLabRanges([...labsDataClone]);
+  };
+
+  const editItemHandler = (item) => {
+    setSelectedRange(item);
+    setShowNewRangeDialog(true);
   };
 
   const dropdownChangeHandler = (index, name, value) => {
-    const labsDataClone = [...labs];
+    const labsDataClone = [...labRanges];
     labsDataClone[index][name] = value;
-    setLabs([...labsDataClone]);
+    setLabRanges([...labsDataClone]);
+  };
+
+  const applyResetHandler = () => {
+    LabRangeService.resetLabRanges().then((res) => {
+      enqueueSnackbar(`${res.message}`, { variant: "success" });
+    });
   };
 
   return (
-    <div className={classes.root}>
-      <Grid
-        container
-        justify="space-between"
-        className={classes.mb2}
-      >
-        <Grid item lg={6}>
-          <Grid container justify="space-between">
-            <Typography
-              component="h1"
-              variant="h2"
-              color="textPrimary"
-              className={classes.title}
-            >
-              Functional Lab Ranges
-            </Typography>
-            <Button
-              variant="outlined"
-              className={classes.w100}
-            >
-              Save
-            </Button>
-            <FormControlLabel
-              control={(
-                <Switch
-                  checked={useFuncRange}
-                  color="primary"
-                  size="medium"
-                  name="switchBox"
-                  onChange={handleChangeFuncRange}
-                  inputProps={{ "aria-label": "secondary checkbox" }}
-                  className={classes.pointerEnable} // enable clicking on switch only
-                />
-              )}
-              label="Use Functional Range"
-              labelPlacement="start"
-              classes={{
-                label: classes.label,
-                root: classes.labelContainer, // to disable clicking of label
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Button
-            variant="outlined"
-            component="label"
-            className={classes.w100}
+    <>
+      <Alert
+        open={showResetDialog}
+        title="Confirm Reset"
+        message="Are you sure you want to reset all custom lab ranges to the original values?"
+        applyButtonText="Reset"
+        cancelButtonText="Cancel"
+        applyForm={applyResetHandler}
+        cancelForm={closeResetDialog}
+      />
+      <NewLabRange
+        isOpen={showNewRangeDialog}
+        onClose={() => setShowNewRangeDialog(false)}
+        reloadData={fetchLabs}
+        selectedItem={selectedRange}
+      />
+      <div className={classes.root}>
+        <Grid item lg={10}>
+          <Grid
+            container
+            justify="space-between"
+            className={classes.mb2}
           >
-            Reset
-          </Button>
-        </Grid>
-      </Grid>
+            <Grid item lg={6}>
+              <Grid container justify="space-between">
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  color="textPrimary"
+                  className={classes.title}
+                >
+                  Functional Lab Ranges
+                </Typography>
+                <Button
+                  variant="outlined"
+                  className={classes.w100}
+                  onClick={() => setShowNewRangeDialog(true)}
+                >
+                  New
+                </Button>
+                <FormControlLabel
+                  control={(
+                    <Switch
+                      checked={useFuncRange}
+                      color="primary"
+                      size="medium"
+                      name="switchBox"
+                      onChange={handleChangeFuncRange}
+                      inputProps={{ "aria-label": "secondary checkbox" }}
+                      className={classes.pointerEnable} // enable clicking on switch only
+                    />
+                  )}
+                  label="Use Functional Range"
+                  labelPlacement="start"
+                  classes={{
+                    label: classes.label,
+                    root: classes.labelContainer, // to disable clicking of label
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="outlined"
+                component="label"
+                className={classes.w100}
+                onClick={() => openResetDialog()}
+              >
+                Reset Values
+              </Button>
+            </Grid>
+          </Grid>
 
-      <Grid container>
-        <Grid item md={9} xs={12}>
           <TableContainer className={classes.mb2}>
             <Table size="small" className={classes.table}>
               <TableHead>
@@ -178,8 +203,8 @@ const LabRanges = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {!!labSearch && labSearchResults.length
-                  ? labSearchResults.map((item, index) => (
+                {labRanges && labRanges.length
+                  ? labRanges.map((item, index) => (
                     <StyledTableRowLg key={item.title}>
                       <TableCell>{item.title}</TableCell>
                       <TableCell>
@@ -221,6 +246,11 @@ const LabRanges = () => {
                         </Button>
                       </TableCell>
                       <TableCell>
+                        <Button variant="outlined" onClick={() => editItemHandler(item)}>
+                          Edit
+                        </Button>
+                      </TableCell>
+                      <TableCell>
                         {item.created ? moment(item.created).format("MMM D YYYY") : ""}
                       </TableCell>
                       <TableCell>
@@ -229,78 +259,21 @@ const LabRanges = () => {
                       <TableCell>{item.created_by}</TableCell>
                     </StyledTableRowLg>
                   ))
-                  : labs && labs.length
-                    ? labs.map((item, index) => (
-                      <StyledTableRowLg key={item.title}>
-                        <TableCell>{item.title}</TableCell>
-                        <TableCell>
-                          <DropDown
-                            options={AgeOptions}
-                            name="age"
-                            label="Age"
-                            value={item.age}
-                            onSelectChange={(value) => dropdownChangeHandler(index, "age", value)}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <DropDown
-                            options={GenderOptions}
-                            name="gender"
-                            label="Gender"
-                            value={item.gender}
-                            onSelectChange={(value) => dropdownChangeHandler(index, "gender", value)}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <Counter />
-                        </TableCell>
-                        <TableCell>
-                          <TextField
-                            type="number"
-                            value={item.high}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <TextField
-                            type="number"
-                            value={item.low}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <Button variant="outlined" onClick={() => deleteItemHandler(index)}>
-                            Delete
-                          </Button>
-                        </TableCell>
-                        <TableCell>
-                          {item.created ? moment(item.created).format("MMM D YYYY") : ""}
-                        </TableCell>
-                        <TableCell>
-                          {item.updated ? moment(item.updated).format("MMM D YYYY") : ""}
-                        </TableCell>
-                        <TableCell>{item.created_by}</TableCell>
-                      </StyledTableRowLg>
-                    ))
-                    : (
-                      <StyledTableRowLg>
-                        <TableCell colSpan={10}>
-                          <Typography align="center" variant="body1">
-                            No Records Found...
-                          </Typography>
-                        </TableCell>
-                      </StyledTableRowLg>
-                    )}
+                  : (
+                    <StyledTableRowLg>
+                      <TableCell colSpan={10}>
+                        <Typography align="center" variant="body1">
+                          No Records Found...
+                        </Typography>
+                      </TableCell>
+                    </StyledTableRowLg>
+                  )}
               </TableBody>
             </Table>
           </TableContainer>
         </Grid>
-      </Grid>
-      <TextField
-        name="labSearch"
-        label="Search Lab Tests"
-        value={labSearch}
-        onChange={(e) => setLabSearch(e.target.value)}
-      />
-    </div>
+      </div>
+    </>
   );
 };
 
