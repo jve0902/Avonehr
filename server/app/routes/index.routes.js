64780276b57f40f591ea464ae6992020d06cc0c0
jve0902 <jay090218@gmail.com>
2021-03-11 22:35:41 +0100

    Add functional range to client

--- server/app/routes/index.routes.js
+++ server/app/routes/index.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/auth/user", [authJwt.verifyToken], IndexController.getUser);
 router.get("/auth/client", [authJwt.verifyToken], IndexController.getClient);
+router.get("/auth/client/functional-range", [authJwt.verifyToken], IndexController.getFunctionalRange);
 router.get("/auth/patient", [authJwt.verifyToken], IndexController.getPatient);
 router.get(
   "/auth/corporate-user",
