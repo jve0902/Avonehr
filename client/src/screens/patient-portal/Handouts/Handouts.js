e27ed7f0ff33a872b36d7b868a48eabeeb01ec91
jve0902 <jay090218@gmail.com>
2021-03-17 21:21:40 +0600

    CLIN-40: Improved code readability and design quality and fixed enqueueSnackbar error issue

--- client/src/screens/patient-portal/Handouts/Handouts.js
+++ client/src/screens/patient-portal/Handouts/Handouts.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect, useCallback } from "react";
 
-import { makeStyles } from "@material-ui/core";
+import {
+  Container,
+  CssBaseline,
+  makeStyles,
+} from "@material-ui/core";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
 import moment from "moment";
 
 import PatientPortalService from "../../../services/patient_portal/patient-portal.service";
-import HandoutDocumentViewer from "./components/modal/HandoutDocumentViewer";
+import HandoutDocumentViewerModal from "./components/modal/HandoutDocumentViewerModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,41 +22,14 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(5),
     paddingBottom: theme.spacing(5),
   },
+  handouts: {
+    marginTop: theme.spacing(3),
+  },
   title: {
-    marginBottom: theme.spacing(1),
+    marginBottom: theme.spacing(1.5),
   },
-  subTitle: {
-    marginBottom: theme.spacing(3.5),
-  },
-  handoutContainer: {
-    display: "flex",
-  },
-  handoutCard: {
-    marginTop: theme.spacing(2.5),
-  },
-  handout: {
-    display: "flex",
-    flexDirection: "column",
-    listStyle: "none",
-    marginRight: theme.spacing(3.2),
-  },
-  handoutText: {
-    fontSize: "13px",
-    display: "flex",
-    justifyContent: "space-between",
-    listStyle: "none",
-    padding: theme.spacing(0),
-    cursor: "pointer",
-    textDecoration: "none",
-    width: "100%",
-    color: theme.palette.text.primary,
-    "&:hover": {
-      background: "#fafafa",
-    },
-  },
-  handoutLabel: {
-    fontWeight: 500,
-    marginBottom: theme.spacing(1),
+  listItem: {
+    paddingLeft: theme.spacing(0),
   },
 }));
 
@@ -55,7 +37,7 @@ const Handouts = () => {
   const classes = useStyles();
   const [handouts, setHandouts] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
-  const [filePath, setFilePath] = useState("");
+  const [selectedFilepath, setSelectedFilepath] = useState(null);
 
   const fetchHandouts = useCallback(() => {
     PatientPortalService.getHandouts().then((res) => {
@@ -67,67 +49,50 @@ const Handouts = () => {
     fetchHandouts();
   }, [fetchHandouts]);
 
-  const getHandoutColumn = (handoutItem, label, index, fileName) => handoutItem && (
-    <ul className={classes.handout}>
-      {index === 0 && (
-        <li className={classes.handoutLabel}>
-          {label}
-        </li>
-      )}
-      <li
-        className={classes.handoutText}
-      >
-        <Typography
-          variant="body1"
-          color="textPrimary"
-          onClick={() => {
-            setFilePath(`${process.env.REACT_APP_API_URL}static/patient/${fileName}`);
-            setIsOpen(true);
-          }}
-        >
-          {label === "Created" ? moment(handoutItem).format("MMM Do YYYY") : handoutItem}
-        </Typography>
-      </li>
-    </ul>
-  );
+  const handleOnClick = (_, filename) => {
+    setSelectedFilepath(`${process.env.REACT_APP_API_URL}static/patient/${filename}`);
+    setIsOpen(true);
+  };
 
   return (
-    <div className={classes.root}>
-      <Typography
-        component="h1"
-        variant="h2"
-        color="textPrimary"
-        className={classes.title}
-      >
-        Handouts
-      </Typography>
-      <Typography
-        variant="h5"
-        color="textPrimary"
-        className={classes.subTitle}
-      >
-        This page is used to view handouts from your provider.
-      </Typography>
-      {
-        handouts.length
-          ? handouts.map((item, index) => (
-            <div className={classes.handoutContainer}>
-              {getHandoutColumn(item?.filename, "Filename", index, item?.fileName)}
-              {getHandoutColumn(item?.created, "Created", index, item?.fileName)}
-            </div>
-          ))
-          : <Typography>No handouts found...</Typography>
-      }
+    <>
+      <CssBaseline />
+      <Container maxWidth={false} className={classes.root}>
+        <Typography component="h1" variant="h2" color="textPrimary" className={classes.title}>
+          Handouts
+        </Typography>
+        <Typography component="h1" variant="h5" color="textPrimary">
+          This page is used to view handouts from your provider.
+        </Typography>
+        <List component="nav" aria-label="main mailbox folders" className={classes.handouts}>
+          {
+            handouts.length
+              ? handouts.map((item) => (
+                <ListItem
+                  button
+                  key={item.id}
+                  onClick={(_) => handleOnClick(_, item.filename)}
+                  className={classes.listItem}
+                >
+                  <ListItemIcon>
+                    <PictureAsPdfIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={`${item?.filename}  ${moment(item?.created).format("MMM Do YYYY")}`}
+                  />
+                </ListItem>
+              ))
+              : <Typography>No handouts found...</Typography>
+          }
+        </List>
+      </Container>
 
-      {filePath && (
-        <HandoutDocumentViewer
-          filePath={filePath}
-          isOpen={isOpen}
-          hendleOnClose={() => setIsOpen(false)}
-        />
-      )}
-
-    </div>
+      <HandoutDocumentViewerModal
+        filePath={selectedFilepath}
+        isOpen={isOpen}
+        hendleOnClose={() => setIsOpen(false)}
+      />
+    </>
   );
 };
 
