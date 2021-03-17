a8eb4b4f1fc58bdfa6692b08c2350d594e8369bb
jve0902 <jay090218@gmail.com>
2021-03-17 08:47:49 +0500

    Added loading flag on client ranges page

--- client/src/screens/Client/Setup/LabRanges/LabRanges.js
+++ client/src/screens/Client/Setup/LabRanges/LabRanges.js
@@ -63,6 +63,7 @@ const LabRanges = () => {
   const [useFuncRange, setUseFuncRange] = useState(true);
   const [labRanges, setLabRanges] = useState([]);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const openDeleteDialog = (item) => {
     setSelectedRange(item);
@@ -93,7 +94,11 @@ const LabRanges = () => {
   const fetchLabRanges = useCallback(() => {
     LabRangeService.getLabRanges().then((res) => {
       setLabRanges(res.data);
-    });
+      setIsLoading(false);
+    })
+      .catch(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   useEffect(() => {
@@ -270,7 +275,7 @@ const LabRanges = () => {
                   <StyledTableRowLg>
                     <TableCell colSpan={10}>
                       <Typography align="center" variant="body1">
-                        No Records Found...
+                        {isLoading ? "Fetching Client Ranges..." : "No Records Found..."}
                       </Typography>
                     </TableCell>
                   </StyledTableRowLg>
