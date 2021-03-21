1dbde80f2d33bfc692c1d672aa29cd2fa0ff534a
jve0902 <jay090218@gmail.com>
2021-03-22 01:58:14 +0600

    CLIN-43: Moved the copyright text of the public pages to the bottom/end of the page

--- client/src/layouts/MainLayout/MainLayout.js
+++ client/src/layouts/MainLayout/MainLayout.js
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 
+import { useMediaQuery } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import clsx from "clsx";
 import PropTypes from "prop-types";
 
 import { Header, Footer, Sidebar } from "./components";
 
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+  },
+  shiftContent: {
+    paddingLeft: 0,
+  },
+  content: {
+    flex: 1,
+  },
+}));
+
+
 const MainLayout = ({ children }) => {
   const [openSidebar, setOpenSidebar] = useState(false);
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"), {
+    defaultMatches: true,
+  });
 
   const handleSidebarOpen = () => {
     setOpenSidebar(true);
@@ -17,14 +42,19 @@ const MainLayout = ({ children }) => {
   };
 
   return (
-    <div className="main-container">
+    <div
+      className={clsx({
+        [classes.root]: true,
+        [classes.shiftContent]: isDesktop,
+      })}
+    >
       <Header onSidebarOpen={handleSidebarOpen} />
       <Sidebar
         onClose={handleSidebarClose}
         open={openSidebar}
         variant="temporary"
       />
-      <Container maxWidth="lg">{children}</Container>
+      <Container maxWidth="lg" className={classes.content}>{children}</Container>
       <Footer />
     </div>
   );
--- client/src/layouts/MainLayout/components/Footer/Footer.js
+++ client/src/layouts/MainLayout/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
@@ -10,27 +10,29 @@ const useStyles = makeStyles((theme) => ({
     clear: "both",
     position: "relative",
     borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    [theme.breakpoints.up("sm")]: {
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
+    marginTop: theme.spacing(5),
   },
 }));
+
+const CustomTypography = withStyles(() => ({
+  root: {
+    fontSize: "11px",
+    lineHeight: "4px",
+    letterSpacing: ".65px",
+  },
+}))(Typography);
 
 export default function Footer() {
   const classes = useStyles();
   return (
     <Container component="footer" maxWidth={false} className={classes.footer}>
-      <Box mt={5}>
-        <Typography variant="body2" color="textSecondary" align="center">
+      <Box pt={2} pb={2}>
+        <CustomTypography variant="body2" color="textSecondary" align="center">
           {"Copyright © "}
           {" "}
           {new Date().getFullYear()}
           {" Clinios"}
-        </Typography>
+        </CustomTypography>
       </Box>
     </Container>
   );
