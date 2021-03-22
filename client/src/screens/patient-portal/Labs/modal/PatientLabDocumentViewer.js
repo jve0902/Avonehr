78f81eb48076c5bbf47cd1552d5159edf21b8b1a
jve0902 <jay090218@gmail.com>
2021-03-22 18:30:33 +0600

    CLIN-39: Fixed a typo

--- client/src/screens/patient-portal/Labs/modal/PatientLabDocumentViewer.js
+++ client/src/screens/patient-portal/Labs/modal/PatientLabDocumentViewer.js
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function checkFileExtension = (fileName) => fileName.substring(fileName.lastIndexOf(".") + 1);
+const checkFileExtension = (fileName) => fileName.substring(fileName.lastIndexOf(".") + 1);
 
 const Lab = ({
   open, documentName, patientId, handleClose,
