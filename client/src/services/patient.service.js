5d8fa4cebaf9e5f09d6d03ce825714b6914b59cd
jve0902 <jay090218@gmail.com>
2021-03-25 14:55:07 +0500

    getMedicationFavorites added in helpers

--- client/src/services/patient.service.js
+++ client/src/services/patient.service.js
@@ -351,6 +351,14 @@ class Patient {
       .then((res) => res.data);
   }
 
+  getMedicationFavorites(patientId) {
+    return axios
+      .get(`${API_BASE}/patient/${patientId}/medication/favorites`, {
+        headers: authHeader(),
+      })
+      .then((res) => res.data);
+  }
+
   getRequisitions(patientId, encounterId) {
     return axios
       .get(`${API_BASE}/patient/${patientId}/requisitions/?encounter_id=${encounterId}`, {
