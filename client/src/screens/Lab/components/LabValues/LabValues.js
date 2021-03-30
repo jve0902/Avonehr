13110503874973e2db83d1185a1b8034113cc536
jve0902 <jay090218@gmail.com>
2021-03-30 22:55:26 +0500

    Integrated graph dialog in labs page values card

--- client/src/screens/Lab/components/LabValues/LabValues.js
+++ client/src/screens/Lab/components/LabValues/LabValues.js
@@ -7,15 +7,26 @@ import {
   TableBody,
   TableRow,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
 import { StyledTableCellSm, StyledTableRowSm } from "../../../../components/common/StyledTable";
 import LabService from "../../../../services/lab.service";
 import { labStatusType, labSourceType } from "../../../../utils/helpers";
+import GraphDialog from "./component/GraphDialog";
+
+const useStyles = makeStyles(() => ({
+  cursorPointer: {
+    cursor: "pointer",
+  },
+}));
 
 const LabValues = (props) => {
+  const classes = useStyles();
   const { labId } = props;
   const [labValues, setLabValues] = useState([]);
+  const [selectedGraph, setSelectedGraph] = useState(null);
+  const [showGraphDialog, setShowGraphDialog] = useState(false);
 
   const fetchLabValues = useCallback(() => {
     LabService.getLabValues(labId).then((res) => {
@@ -27,35 +38,57 @@ const LabValues = (props) => {
     fetchLabValues();
   }, [fetchLabValues]);
 
+  const toggleGraphDialog = () => {
+    setShowGraphDialog((prevState) => !prevState);
+  };
+
+  const rowClickHandler = (row) => {
+    setSelectedGraph(row.filename);
+    toggleGraphDialog();
+  };
+
   return (
-    <TableContainer>
-      <Table size="small" aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCellSm>Test</StyledTableCellSm>
-            <StyledTableCellSm>Result</StyledTableCellSm>
-            <StyledTableCellSm>Conventional Range</StyledTableCellSm>
-            <StyledTableCellSm>Conventional Flag</StyledTableCellSm>
-            <StyledTableCellSm>Functional Range</StyledTableCellSm>
-            <StyledTableCellSm>Functional Flag</StyledTableCellSm>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {labValues.map((row) => (
-            <StyledTableRowSm key={row.type}>
-              <StyledTableCellSm component="th" scope="row">
-                {row.created}
-              </StyledTableCellSm>
-              <StyledTableCellSm>{row.lastFour}</StyledTableCellSm>
-              <StyledTableCellSm>{labStatusType(row.status)}</StyledTableCellSm>
-              <StyledTableCellSm>{labSourceType(row.type)}</StyledTableCellSm>
-              <StyledTableCellSm>{row.assigned_to}</StyledTableCellSm>
-              <StyledTableCellSm>{row.patient_name}</StyledTableCellSm>
-            </StyledTableRowSm>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {!!showGraphDialog && (
+        <GraphDialog
+          fileName={selectedGraph}
+          isOpen={showGraphDialog}
+          onClose={toggleGraphDialog}
+        />
+      )}
+      <TableContainer>
+        <Table size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCellSm>Test</StyledTableCellSm>
+              <StyledTableCellSm>Result</StyledTableCellSm>
+              <StyledTableCellSm>Conventional Range</StyledTableCellSm>
+              <StyledTableCellSm>Conventional Flag</StyledTableCellSm>
+              <StyledTableCellSm>Functional Range</StyledTableCellSm>
+              <StyledTableCellSm>Functional Flag</StyledTableCellSm>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {labValues.map((row) => (
+              <StyledTableRowSm
+                key={row.type}
+                className={classes.cursorPointer}
+                onClick={() => rowClickHandler(row)}
+              >
+                <StyledTableCellSm component="th" scope="row">
+                  {row.created}
+                </StyledTableCellSm>
+                <StyledTableCellSm>{row.lastFour}</StyledTableCellSm>
+                <StyledTableCellSm>{labStatusType(row.status)}</StyledTableCellSm>
+                <StyledTableCellSm>{labSourceType(row.type)}</StyledTableCellSm>
+                <StyledTableCellSm>{row.assigned_to}</StyledTableCellSm>
+                <StyledTableCellSm>{row.patient_name}</StyledTableCellSm>
+              </StyledTableRowSm>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
