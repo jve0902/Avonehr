3bbbcfcae05b07e433c9632b25373de38dd0cd21
jve0902 <jay090218@gmail.com>
2021-03-31 15:49:38 +0600

    Fix lab.png APIs

--- server/app/routes/process-lab.routes.js
+++ server/app/routes/process-lab.routes.js
@@ -5,29 +5,25 @@ const ProcessLab = require("../controllers/process-lab.controller.js");
 const router = express.Router();
 
 router.get("/lab/assign-user", [authJwt.verifyToken], ProcessLab.getAssignUser);
-router.get("/lab/:userId/:labId", [authJwt.verifyToken], ProcessLab.getLabById);
-router.get("/lab/:userId", [authJwt.verifyToken], ProcessLab.getAll);
-router.post("/lab", [authJwt.verifyToken], ProcessLab.createLab);
-router.put("/lab/:labId", [authJwt.verifyToken], ProcessLab.updateLab);
-router.put(
-  "/lab/update/:labId",
-  [authJwt.verifyToken],
-  ProcessLab.updateLabData
-);
 router.get(
-  "/lab/histroy/:labId",
+  "/lab/values/:labId",
   [authJwt.verifyToken],
-  ProcessLab.getLabHistory
+  ProcessLab.getLabValues
 );
 router.get(
   "/lab/user-history/:userId",
   [authJwt.verifyToken],
   ProcessLab.getLabUserHistory
 );
+router.get("/lab/:userId/:labId", [authJwt.verifyToken], ProcessLab.getLabById);
+router.get("/lab/:userId", [authJwt.verifyToken], ProcessLab.getAll);
+router.post("/lab", [authJwt.verifyToken], ProcessLab.createLab);
+router.put("/lab/:labId", [authJwt.verifyToken], ProcessLab.updateLab);
 router.get(
-  "/lab/values/:labId",
+  "/lab/histroy/:labId",
   [authJwt.verifyToken],
-  ProcessLab.getLabValues
+  ProcessLab.getLabHistory
 );
+
 
 module.exports = router;
