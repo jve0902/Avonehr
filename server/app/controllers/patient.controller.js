67bc947d6dd70ef0342517a5b1268639ee8c5e23
jve0902 <jay090218@gmail.com>
2021-04-01 19:25:07 +0500

    added drug_strength_id in medications APIs

--- server/app/controllers/patient.controller.js
+++ server/app/controllers/patient.controller.js
@@ -1998,7 +1998,7 @@ const getMedicationById = async (req, res) => {
 
   try {
     const dbResponse = await db.query(
-      `select d.id, d.name, ds.strength, ds.unit, ds.form
+      `select d.id, d.name, ds.id drug_strength_id, ds.strength, ds.unit, ds.form
       , df.descr frequency, pd.start_dt, pd.expires, pd.amount, pd.refills
       , pd.generic, pd.patient_instructions, pd.pharmacy_instructions
       from patient_drug pd
@@ -2060,7 +2060,7 @@ const getMedicationRecents = async (req, res) => {
 
   try {
     const dbResponse = await db.query(
-      `select d.id, d.name, ds.strength, ds.unit, ds.form, df.descr frequency, pd.expires, pd.amount, pd.refills, pd.generic
+      `select d.id, d.name, ds.id drug_strength_id, ds.strength, ds.unit, ds.form, df.descr frequency, pd.expires, pd.amount, pd.refills, pd.generic
       from patient_drug pd
       left join drug d on d.id=pd.drug_id
       left join drug_strength ds on ds.drug_id=pd.drug_id
