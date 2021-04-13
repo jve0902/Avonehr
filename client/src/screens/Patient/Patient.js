cd81d3a7de0d403e3dd5fd5778e83ecb9448f5f1
jve0902 <jay090218@gmail.com>
2021-04-13 11:31:45 +0500

    updated requisition dialog height

--- client/src/screens/Patient/Patient.js
+++ client/src/screens/Patient/Patient.js
@@ -1178,7 +1178,7 @@ const Patient = () => {
           applyForm={() => dispatch(toggleRequisitionDialog())}
           cancelForm={() => dispatch(toggleRequisitionDialog())}
           hideActions
-          size="xl"
+          size="lg"
         />
       )}
 
