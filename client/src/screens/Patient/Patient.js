2ea109b75ce1c7724bc530231f0197d0f24a85e1
jve0902 <jay090218@gmail.com>
2021-04-14 11:48:27 +0500

    passed contentToggleHandler prop for diagnoses prop

--- client/src/screens/Patient/Patient.js
+++ client/src/screens/Patient/Patient.js
@@ -1315,6 +1315,15 @@ const Patient = () => {
                     item.title,
                   )}
                   updateMinHeight={updateMinHeight}
+                  contentToggleHandler={(value) => {
+                    // setFetchDiagnosesStatus(value);
+                    dispatch(setDiagnosesStatus(value));
+                    if (value) {
+                      fetchDiagnoses(value);
+                    } else {
+                      fetchDiagnoses();
+                    }
+                  }}
                 />
               </Grid>
             ))}
@@ -1340,15 +1349,6 @@ const Patient = () => {
                       ? `Balance $${patientBalance}`
                       : ""
                   }
-                  contentToggleHandler={(value) => {
-                    // setFetchDiagnosesStatus(value);
-                    dispatch(setDiagnosesStatus(value));
-                    if (value) {
-                      fetchDiagnoses(value);
-                    } else {
-                      fetchDiagnoses();
-                    }
-                  }}
                 />
               </Grid>
             ))}
