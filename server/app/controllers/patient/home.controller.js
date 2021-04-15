6d0d290c1cf51b581558e67b05b38a02545799a2
jve0902 <jay090218@gmail.com>
2021-04-15 15:32:51 +0600

    Added uc.user_id field in getUpcomingAppointments route controller from the server

--- server/app/controllers/patient/home.controller.js
+++ server/app/controllers/patient/home.controller.js
@@ -78,7 +78,7 @@ const getUpcomingAppointments = async (req, res) => {
   let $sql;
 
   try {
-    $sql = `select uc.patient_id, uc.start_dt, uc.end_dt, uc.status, concat(u.firstname, ' ', u.lastname) provider
+    $sql = `select uc.patient_id, uc.user_id, uc.start_dt, uc.end_dt, uc.status, concat(u.firstname, ' ', u.lastname) provider
     from user_calendar uc
     join user u on u.id=uc.user_id
     where uc.patient_id=${patient_id}
