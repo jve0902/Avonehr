9a35eb1be9342f22bace11dc21fbb4e852521fd9
jve0902 <jay090218@gmail.com>
2021-04-18 02:56:33 +0600

    CLIN-83: Patient Portal - made copyright notice at bottom of page same size as doctor portal

--- client/src/layouts/PatientPortal/components/Footer/Footer.js
+++ client/src/layouts/PatientPortal/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
 import useAuth from "../../../../hooks/useAuth";
@@ -10,44 +10,34 @@ import useAuth from "../../../../hooks/useAuth";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    flex: 0,
-    // backgroundColor: theme.palette.primary.main,
-    color: "#ffffff",
     clear: "both",
     position: "relative",
     borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-    backgroundColor: "#fafafa",
-    [theme.breakpoints.up("sm")]: {
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
-  },
-  footerText: {
-    color: theme.palette.secondary.main,
-    marginTop: 0,
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    "& p": {
-      color: theme.palette.secondary.main,
-    },
+    marginTop: theme.spacing(5),
   },
 }));
+
+const CustomTypography = withStyles((theme) => ({
+  root: {
+    fontSize: "11px",
+    lineHeight: "4px",
+    letterSpacing: ".65px",
+    color: theme.palette.secondary.main,
+  },
+}))(Typography);
 
 export default function Footer() {
   const classes = useStyles();
   const { user } = useAuth();
   return (
     <Container component="footer" maxWidth={false} className={classes.footer}>
-      <Box mt={5} className={classes.footerText}>
-        <Typography variant="body2" color="textPrimary" align="center">
+      <Box pt={2} pb={2}>
+        <CustomTypography variant="body2" color="textPrimary" align="center">
           {"Copyright © "}
           {" "}
           {new Date().getFullYear()}
           {` Clinios - User ${user.firstname} ${user.lastname}`}
-        </Typography>
+        </CustomTypography>
       </Box>
     </Container>
   );
