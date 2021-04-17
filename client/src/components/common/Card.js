3542a39b410c6c8600b8c10e63294b77f9757af5
jve0902 <jay090218@gmail.com>
2021-04-17 15:23:30 +0500

    Fixed graph icon styling

--- client/src/components/common/Card.js
+++ client/src/components/common/Card.js
@@ -12,11 +12,11 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/AddCircleOutline";
-import CancelIcon from "@material-ui/icons/Cancel";
-import SaveIcon from "@material-ui/icons/CheckCircle";
-import CardIcon from "@material-ui/icons/CreditCard";
-import DesktopIcon from "@material-ui/icons/DesktopMac";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CancelIcon from "@material-ui/icons/CancelOutlined";
+import CardIcon from "@material-ui/icons/CreditCardOutlined";
+import DesktopIcon from "@material-ui/icons/DesktopMacOutlined";
+import MoreVertIcon from "@material-ui/icons/MoreVertOutlined";
+import SaveIcon from "@material-ui/icons/SaveOutlined";
 import { mdiCalendarBlankOutline, mdiChartLine } from "@mdi/js";
 import Icon from "@mdi/react";
 import clsx from "clsx";
@@ -96,6 +96,7 @@ const useStyles = makeStyles((theme) => ({
   graphIcon: {
     cursor: "pointer",
     marginRight: "auto",
+    marginLeft: theme.spacing(1),
   },
   textField: {
     height: 8,
@@ -183,7 +184,6 @@ const PatientCard = (props) => {
         >
           <Typography className={classes.title}>
             {title}
-            &nbsp; &nbsp;
           </Typography>
           {title === "All Tests" && (
             <>
@@ -306,16 +306,16 @@ PatientCard.defaultProps = {
   secondaryButtonText: "Edit",
   icon: null,
   cardInfo: null,
-  primaryButtonHandler: () => {},
-  secondaryButtonHandler: () => {},
-  iconHandler: () => {},
-  searchHandler: () => {},
-  editorSaveHandler: () => {},
-  editorCancelHandler: () => {},
-  updateLayoutHandler: () => {},
-  resetLayoutHandler: () => {},
+  primaryButtonHandler: () => { },
+  secondaryButtonHandler: () => { },
+  iconHandler: () => { },
+  searchHandler: () => { },
+  editorSaveHandler: () => { },
+  editorCancelHandler: () => { },
+  updateLayoutHandler: () => { },
+  resetLayoutHandler: () => { },
   isLayoutUpdated: false,
-  contentToggleHandler: () => {},
+  contentToggleHandler: () => { },
   hasMinHeight: false,
 };
 
