401073cfae8e89277a96ed0d1a44f14fdf2db2f8
jve0902 <jay090218@gmail.com>
2021-04-26 23:26:23 +0500

    Fixed linter

--- client/src/screens/Client/Manage/AccountingSearch/components/AccountingSearchResults.js
+++ client/src/screens/Client/Manage/AccountingSearch/components/AccountingSearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { makeStyles, withStyles } from "@material-ui/core"
+import { makeStyles, withStyles } from "@material-ui/core";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
