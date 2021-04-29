c74e408ef5522c2141b57f5e0a92eb8eec3bc9ad
jve0902 <jay090218@gmail.com>
2021-04-29 09:31:53 +0600

    CLIN-136: for doctor portal if user is not logged in then redirecting to doctor portal login page

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,41 +1,17 @@
 import React from "react";
 
-import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import { Redirect } from "react-router-dom";
 
 import useAuth from "../hooks/useAuth";
 
-
-const useStyles = makeStyles((theme) => ({
-  root: { paddingTop: theme.spacing(3), minHeight: 500 },
-}));
-
 const Home = () => {
-  const classes = useStyles();
   const { isAuthenticated } = useAuth();
 
   // TODO: This might be changed as per requirements
   if (isAuthenticated) {
     return <Redirect to="/dashboard" />;
   }
-  return (
-    <>
-      <CssBaseline />
-      <Container maxWidth="lg" className={classes.root}>
-        <Typography
-          component="h1"
-          variant="h2"
-          color="textPrimary"
-          className={classes.pageTitle}
-        >
-          Home
-        </Typography>
-      </Container>
-    </>
-  );
+  return <Redirect to="/login_client" />;
 };
 
 export default Home;
