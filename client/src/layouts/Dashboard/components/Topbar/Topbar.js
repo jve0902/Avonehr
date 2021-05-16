d0c813129b92efbf0b3670d6fc2eede4b91658c5
jve0902 <jay090218@gmail.com>
2021-05-16 10:58:01 -0700

    .

--- client/src/layouts/Dashboard/components/Topbar/Topbar.js
+++ client/src/layouts/Dashboard/components/Topbar/Topbar.js
@@ -201,7 +201,7 @@ const Topbar = (props) => {
         <div className={classes.headerWithNav}>
           <Typography className={classes.title} variant="h6" noWrap>
             <RouterLink to={user.login_url || "/dashboard"} className={classes.titleAsLogo}>
-              Avon
+              AvonEMR
             </RouterLink>
           </Typography>
           <Hidden mdDown>
