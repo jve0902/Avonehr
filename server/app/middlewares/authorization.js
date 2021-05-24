98a6b9cd0a7cd2c3551d2eefe9b57008f87262bc
jve0902 <jay090218@gmail.com>
2021-05-24 17:36:59 +0600

    change read only message

--- server/app/middlewares/authorization.js
+++ server/app/middlewares/authorization.js
@@ -1,7 +1,7 @@
 const isReadOnly = (req, res, next) => {
   const readOnly = process.env.READ_ONLY === "true";
   if (readOnly) {
-    return res.status(403).send({ message: "App is on read only mode. Your can not insert, update or delete!" });
+    return res.status(403).send({ message: "App is on read only mode. Your can not add, update or delete!" });
   }
     next();
 };
