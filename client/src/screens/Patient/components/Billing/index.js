d59718bb0a2bf8a202c61bd3ff78c84d0a8f8b49
jve0902 <jay090218@gmail.com>
2021-06-15 17:16:14 +0500

    Added NewBillingDialog component

--- client/src/screens/Patient/components/Billing/index.js
+++ client/src/screens/Patient/components/Billing/index.js
@@ -2,3 +2,4 @@ export { default as NewTransactionForm } from "./NewTransactionForm";
 export { default as PaymentForm } from "./PaymentForm";
 export { default as BillingCardContent } from "./Content";
 export { default as BillingDetails } from "./Details";
+export { default as NewBillingDialog } from "./NewBillingDialog";
