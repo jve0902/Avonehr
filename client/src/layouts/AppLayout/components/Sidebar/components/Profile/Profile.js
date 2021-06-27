dca158c15225110eebd667cd10dcfc1ad68a52c5
jve0902 <jay090218@gmail.com>
2021-06-27 23:44:25 +0500

    Removed user info from sidebar - added logo

--- client/src/layouts/AppLayout/components/Sidebar/components/Profile/Profile.js
+++ client/src/layouts/AppLayout/components/Sidebar/components/Profile/Profile.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-import { Avatar, Typography } from "@material-ui/core";
-import Divider from "@material-ui/core/Divider";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import InputIcon from "@material-ui/icons/InputOutlined";
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import { Link as RouterLink, useHistory } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
-import useAuth from "../../../../../../hooks/useAuth";
+import Logo from "../../../../../../assets/img/Logo.svg";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexDirection: "column",
@@ -20,21 +16,8 @@ const useStyles = makeStyles((theme) => ({
     minHeight: "fit-content",
   },
   avatar: {
-    width: 60,
-    height: 60,
-  },
-  name: {
-    marginTop: theme.spacing(1),
-  },
-  authCredential: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: theme.spacing(2),
-  },
-  signOutButton: {
-    marginLeft: theme.spacing(1),
-    backgroundColor: fade(theme.palette.secondary.light, 0.15),
+    width: 175,
+    height: 70,
   },
 }));
 
@@ -44,17 +27,6 @@ const Profile = (props) => {
   } = props;
 
   const classes = useStyles();
-  const history = useHistory();
-  const { user, logout } = useAuth();
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-      history.push(user.login_url || "/login_client");
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   return (
     <div {...rest} className={clsx(classes.root, className)}>
@@ -62,27 +34,9 @@ const Profile = (props) => {
         alt="Person"
         className={classes.avatar}
         component={RouterLink}
-        src={(user && user.avatar) || ""}
-        to="/settings"
+        src={Logo}
+        to="/"
       />
-      <Typography className={classes.name} variant="h4">
-        {user && `${user.firstname} ${user.lastname}`}
-      </Typography>
-      <Typography variant="body2">{user && user.bio}</Typography>
-      <>
-        <Divider className={classes.divider} />
-        <Hidden lgUp>
-          <div className={classes.authCredential}>
-            <IconButton
-              className={classes.signOutButton}
-              color="inherit"
-              onClick={(event) => handleLogout(event, logout)}
-            >
-              <InputIcon />
-            </IconButton>
-          </div>
-        </Hidden>
-      </>
     </div>
   );
 };
