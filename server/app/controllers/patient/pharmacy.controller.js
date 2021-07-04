9731763403b7143b18d2af330e9058c88e620756
jve0902 <jay090218@gmail.com>
2021-07-04 11:04:55 +0600

    Fix patient pharmacy  controllers

--- server/app/controllers/patient/pharmacy.controller.js
+++ server/app/controllers/patient/pharmacy.controller.js
@@ -1,4 +1,4 @@
-const { configuration, makeDb } = require("../../db/db.js");
+const db = require("../../db");
 const {
   errorMessage,
   successMessage,
@@ -6,17 +6,14 @@ const {
 } = require("../../helpers/status");
 
 const getPharmacy = async (req, res) => {
-  const db = makeDb(configuration, res);
   let { patient_id } = req.query;
 
   if (typeof patient_id === "undefined") {
     patient_id = req.user_id;
   }
 
-  let $sql;
-
   try {
-    $sql = `select p.id, p.firstname, p.middlename, p.lastname, p.gender, p.dob, p.ssn, p.preferred_name,
+   const $sql = `select p.id, p.firstname, p.middlename, p.lastname, p.gender, p.dob, p.ssn, p.preferred_name,
      p.referred_by, p.phone_home, p.phone_cell, p.phone_work, p.email, p.client_id
     , ph.id, ph.name, ph.address, ph.address2, ph.city, ph.state, ph.postal, ph.country, ph.phone, ph.fax
     , ph2.id pharmacy2_id, ph2.name pharmacy2_name, ph2.address pharmacy2_address, ph2.address2 pharmacy2_address2, ph2.city pharmacy2_city,
@@ -24,81 +21,73 @@ const getPharmacy = async (req, res) => {
     from patient p
     left join pharmacy ph on ph.id=p.pharmacy_id
     left join pharmacy ph2 on ph2.id=p.pharmacy2_id
-    where p.id=?`;
+    where p.id=$1`;
 
     const dbResponse = await db.query($sql, [patient_id]);
 
-    if (!dbResponse) {
+    if (!dbResponse.rows) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
+//TODO:: This endpoint might not in use. Need to be checked and removed.
 const updatePharmacy = async (req, res) => {
   const { id } = req.params;
   const formData = req.body.data;
   formData.updated = new Date();
   formData.updated_user_id = req.user_id;
 
-  const db = makeDb(configuration, res);
   try {
     const updateResponse = await db.query(
-      `update patient set ? where id=?`,
+      `update patient set $1 where id=$2`,
       [formData, id]
     );
 
-    if (!updateResponse.affectedRows) {
+    if (!updateResponse.rowCount) {
       errorMessage.message = "Update not successful";
       return res.status(status.notfound).send(errorMessage);
     }
 
-    successMessage.data = updateResponse;
+    successMessage.data = updateResponse.rows;
     successMessage.message = "Update successful";
     return res.status(status.created).send(successMessage);
   } catch (err) {
     console.log("err", err);
     errorMessage.message = "Update not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
 const searchPharmacy = async (req, res) => {
   const { text } = req.body.data;
-
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
       `select id, name, address, address2, city, state, postal, country, phone, fax
       from pharmacy
-      where name like '${text}%'
+      where name ilike '${text}%'
       order by name
       limit 20
       `
     );
 
-    if (!dbResponse) {
+    if (!dbResponse.rows) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
 
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
     console.log("err", err);
     errorMessage.message = "Search not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
