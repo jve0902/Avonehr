604c438537d484ca3c324da146967641f530cea7
jve0902 <jay090218@gmail.com>
2021-07-05 20:31:53 +0500

    fixed linter

--- client/src/screens/Catalog/Catalog.js
+++ client/src/screens/Catalog/Catalog.js
@@ -5,7 +5,7 @@ import {
   TableContainer, Table, TableRow, TableBody, TableHead,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import InfoIcon from '@material-ui/icons/InfoOutlined';
+import InfoIcon from "@material-ui/icons/InfoOutlined";
 
 import { StyledTableRowSm, StyledTableCellSm } from "../../components/common/StyledTable";
 import HoverPopover from "../../components/HoverPopover";
