5c6bcbe55d03f0bcdd95476b099cb90454d99f38
jve0902 <jay090218@gmail.com>
2021-07-07 19:49:52 +0600

    Fix myself controllers

--- server/app/controllers/myself.controller.js
+++ server/app/controllers/myself.controller.js
@@ -1,28 +1,26 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const moment = require("moment");
-const { configuration, makeDb } = require("../db/db.js");
+const db = require("../db");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
 const getProfile = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
       `select firstname, lastname, email, title, created, email_forward_user_id, phone, status, timezone
-      from user where id=${req.params.userId}`
+      from users where id=${req.params.userId}`
     );
 
-    if (!dbResponse || dbResponse === 0) {
+    if (!dbResponse || dbResponse.rows.length === 0) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse[0];
+    successMessage.data = dbResponse.rows[0];
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
@@ -33,90 +31,91 @@ const updateProfile = async (req, res) => {
     return res.status(status.error).send(errorMessage);
   }
 
-  const db = makeDb(configuration, res);
-  const { data } = req.body;
-
-  data.updated = moment().format("YYYY-MM-DD HH:mm:ss");
-  data.updated_user_id = req.user_id;
-
-  // Convert date format
-  data.created = moment(data.created).format("YYYY-MM-DD HH:mm:ss");
-
-  // Hash the password
-  if (data.password) data.password = bcrypt.hashSync(data.password, 8);
+  const { firstname, lastname, email, title, email_forward_user_id, phone, password, created } = req.body.data;
 
   try {
-    const updateResponse = await db.query(`update user set ? where id =?`,
-      [data, req.params.userId]
-    );
+    let $sql;
+    $sql = `update users set firstname='${firstname}', lastname='${lastname}', email='${email}', title='${title}'`;
 
-    if (!updateResponse.affectedRows) {
+    if(email_forward_user_id){
+      $sql += `, email_forward_user_id=${email_forward_user_id}`;
+    }
+    if(phone){
+      $sql += `, phone='${phone}'`;
+    }
+    if(password){
+      $sql += `, password=${bcrypt.hashSync(password, 8)}`;
+    }
+    if(created){
+      $sql += `, created='${moment(created).format("YYYY-MM-DD HH:mm:ss")}'`;
+    }
+    
+    $sql += `, updated='${moment().format('YYYY-MM-DD hh:ss')}', updated_user_id=${req.user_id} where id =${req.params.userId} RETURNING id`;
+
+
+    const updateResponse = await db.query($sql);
+
+    if (!updateResponse.rowCount) {
       errorMessage.message = "Update not successful";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = updateResponse;
+    successMessage.data = updateResponse.rows;
     successMessage.message = "Update successful";
     return res.status(status.success).send(successMessage);
   } catch (error) {
+    console.log(error);
     errorMessage.message = "Update not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
 const getForwardEmail = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
-      `select u.id, concat(u.firstname, ' ', u.lastname) name
-      from user u where u.client_id=${req.client_id}
-      and u.id<>? order by name limit 100`, [req.params.userId]
+      `select u.id, concat(u.firstname, ' ', u.lastname) AS name
+      from users u where u.client_id=${req.client_id}
+      and u.id<>$1 order by name limit 100`, [req.params.userId]
     );
 
     if (!dbResponse) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
 const getLogins = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
-      `select dt, ip from user_login where user_id=? order by dt desc limit 20`, [req.params.userId]
+      `select dt, ip from user_login where user_id=$1 order by dt desc limit 20`, [req.params.userId]
     );
 
     if (!dbResponse) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
 const getActivityHistory = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
       `select ul.dt, concat(p.firstname, ' ', p.lastname) patient, p.id patient_id, ul.action
       from user_log ul 
       left join patient p on p.id=ul.patient_id
-      where ul.user_id=? order by ul.dt desc limit 20`,
+      where ul.user_id=$1 order by ul.dt desc limit 20`,
       [req.params.userId]
     );
 
@@ -124,13 +123,12 @@ const getActivityHistory = async (req, res) => {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
