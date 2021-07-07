3cefe1c918b583efabca0dbd6be247a7368d3e67
jve0902 <jay090218@gmail.com>
2021-07-07 10:58:31 +0600

    Fix setup ICD codes controller

--- server/app/controllers/icd.controller.js
+++ server/app/controllers/icd.controller.js
@@ -1,20 +1,20 @@
 const { validationResult } = require("express-validator");
-const { configuration, makeDb } = require("../db/db.js");
+const moment = require("moment");
+const db = require("../db");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
 const search = async (req, res) => {
-  const db = makeDb(configuration, res);
   const { searchTerm, checkBox } = req.body;
   let $sql;
 
   try {
     $sql = `select i.id, i.name, ci.favorite, ci.updated, concat(u.firstname, ' ', u.lastname) updated_name
-            from icd i
-            left join client_icd ci on ci.icd_id=i.id and ci.client_id=${req.client_id}
-            left join user u on u.id=ci.updated_user_id
-            where 1 \n`;
+      from icd i
+      left join client_icd ci on ci.icd_id=i.id and ci.client_id=${req.client_id}
+      left join users u on u.id=ci.updated_user_id
+      where true \n`;
     if (searchTerm) {
-      $sql += `and i.name like '${searchTerm}%' \n`;
+      $sql += `and i.name ilike '${searchTerm}%' \n`;
     }
     if (checkBox === true) {
       $sql += `and ci.favorite = true \n`;
@@ -29,13 +29,12 @@ const search = async (req, res) => {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err)
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
@@ -45,20 +44,14 @@ const addFavorite = async (req, res) => {
     errorMessage.message = errors.array();
     return res.status(status.bad).send(errorMessage);
   }
-  const db = makeDb(configuration, res);
-  const client_icd = req.body;
-  client_icd.client_id = req.client_id;
-  client_icd.icd_id = req.body.icd_id;
-  client_icd.favorite = true;
-  client_icd.created = new Date();
-  client_icd.created_user_id = req.user_id;
-  client_icd.updated = new Date();
-  client_icd.updated_user_id = req.user_id;
+
+  const { icd_id }= req.body.data
 
   try {
     const dbResponse = await db.query(
-      "insert into client_icd set ?",
-      client_icd
+      `insert into client_icd(client_id, icd_id, favorite, created, created_user_id) 
+      VALUES(${req.client_id}, $1, true, '${moment().format('YYYY-MM-DD hh:ss')}', ${req.user_id}) RETURNING client_id, icd_id`,
+      [icd_id]
     );
 
     if (!dbResponse) {
@@ -66,14 +59,13 @@ const addFavorite = async (req, res) => {
       res.status(status.notfound).send(errorMessage);
     }
 
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     successMessage.message = "Creation successful";
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Creation not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
@@ -83,23 +75,23 @@ const deleteFavorite = async (req, res) => {
     errorMessage.message = errors.array();
     return res.status(status.error).send(errorMessage);
   }
-  const db = makeDb(configuration, res);
+  const { id } = req.params;
+
   try {
     const deleteResponse = await db.query(
-      `delete from client_icd where client_id=${req.client_id} and icd_id='${req.params.id}'`
+      `DELETE FROM client_icd WHERE client_id=${req.client_id} AND icd_id=$1 RETURNING client_id, icd_id`, [id]
     );
-    if (!deleteResponse.affectedRows) {
+    if (!deleteResponse.rowCount) {
       errorMessage.message = "Deletion not successful";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = deleteResponse;
+    successMessage.data = deleteResponse.rows;
     successMessage.message = "Deletion successful";
     return res.status(status.success).send(successMessage);
   } catch (error) {
+    console.log(error);
     errorMessage.message = "Deletion not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
