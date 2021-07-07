258479d19dd344287250d7ae411cf7a31b4f1c88
jve0902 <jay090218@gmail.com>
2021-07-07 16:11:29 +0600

    Fix setup integrations controller

--- server/app/controllers/integrations.controller.js
+++ server/app/controllers/integrations.controller.js
@@ -1,9 +1,9 @@
 const { validationResult } = require("express-validator");
-const { configuration, makeDb } = require("../db/db.js");
+const moment = require('moment');
+const db = require("../db");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
 const getIntegrations = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(
       `select id, labcorp_api_key, quest_api_key, doctors_data_username, doctors_data_password, stripe_api_key
@@ -15,13 +15,12 @@ const getIntegrations = async (req, res) => {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
@@ -32,30 +31,26 @@ const update = async (req, res) => {
     return res.status(status.error).send(errorMessage);
   }
 
-  const db = makeDb(configuration, res);
-  const client = req.body.data;
-
-  client.updated = new Date();
-  client.updated_user_id = req.user_id;
+  const { labcorp_api_key, quest_api_key, doctors_data_username, doctors_data_password, stripe_api_key} = req.body.data;
 
   try {
     const updateResponse = await db.query(
-      `update client set ? where id =${req.client_id}`,
-      [client]
+      `update client set labcorp_api_key=$1, quest_api_key=$2, doctors_data_username=$3, doctors_data_password=$4,
+       stripe_api_key=$5, updated=$6, updated_user_id=$7 where id=$8 RETURNING id`,
+      [labcorp_api_key, quest_api_key, doctors_data_username, doctors_data_password, stripe_api_key, moment().format('YYYY-MM-DD hh:ss'), req.user_id, req.client_id]
     );
 
-    if (!updateResponse.affectedRows) {
+    if (!updateResponse.rowCount) {
       errorMessage.message = "Update not successful";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = updateResponse;
+    successMessage.data = updateResponse.rows;
     successMessage.message = "Update successful";
     return res.status(status.success).send(successMessage);
   } catch (error) {
+    console.log(error);
     errorMessage.message = "Update not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
