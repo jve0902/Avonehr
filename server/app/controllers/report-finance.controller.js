156a396af941a79a9bc06c9b29f16757e4980288
jve0902 <jay090218@gmail.com>
2021-07-08 11:52:00 +0600

    Fix report finance controller

--- server/app/controllers/report-finance.controller.js
+++ server/app/controllers/report-finance.controller.js
@@ -1,11 +1,11 @@
-const { configuration, makeDb } = require("../db/db.js");
+const db = require("../db");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
 const getAll = async (req, res) => {
-  const db = makeDb(configuration, res);
+  const { from, to } = req.query;
   try {
     const dbResponse = await db.query(
-      `select year(dt) year, month(dt) month
+      `select extract(year from dt) as year, extract(month from dt) as month
       , sum(amount) Total
       , sum(case when type_id=1 then amount end) Service
       , sum(case when type_id=2 then amount end) Credit
@@ -13,24 +13,22 @@ const getAll = async (req, res) => {
       , sum(case when type_id=4 then amount end) Refund
       from tran
       where client_id=${req.client_id}
-      and dt between '${req.query.from}' and '${req.query.to}'
-      group by year(dt), month(dt)
-      order by year(dt), month(dt)
-      limit 100
-      `
-    );
+      and dt between '${from}' and '${to}'
+      group by (extract (year from dt), extract (month from dt))
+      order by (extract (year from dt), extract (month from dt))
+      limit 100`
+    )
 
-    if (!dbResponse) {
+    if (!dbResponse || dbResponse.rows.length === 0) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (err) {
+    console.log(err);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
