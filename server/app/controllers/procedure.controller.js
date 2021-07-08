0fe773f7c502e3e059d2859d5355fa650f4d84fc
jve0902 <jay090218@gmail.com>
2021-07-08 21:10:37 +0600

    Fix procedure controller

--- server/app/controllers/procedure.controller.js
+++ server/app/controllers/procedure.controller.js
@@ -1,9 +1,8 @@
 const { validationResult } = require("express-validator");
-const { configuration, makeDb } = require("../db/db.js");
+const db= require("../db");
 const { errorMessage, successMessage, status } = require("../helpers/status");
 
 const getLabCompnayList = async (req, res) => {
-  const db = makeDb(configuration, res);
   try {
     const dbResponse = await db.query(`select id, name 
     from lab_company 
@@ -11,22 +10,20 @@ const getLabCompnayList = async (req, res) => {
     limit 100
     `);
 
-    if (!dbResponse) {
+    if (!dbResponse || dbResponse.rows.length === 0) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (error) {
+    console.log(error);
     errorMessage.message = "Select not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
 const search = async (req, res) => {
-  const db = makeDb(configuration, res);
   const {
     procedureId,
     procedureDescription,
@@ -40,20 +37,20 @@ const search = async (req, res) => {
   try {
     $sql = `select c.id, c.name proc, lc.name lab_company, cc.favorite, cc.billable, cc.fee, cl.name client_name
         , cc.updated, concat(u.firstname, ' ', u.lastname) updated_name
-        , group_concat(ci.quest_id order by ci.quest_id separator ", ") procedure_group
+        , ARRAY_TO_STRING(ARRAY_AGG(ci.quest_id order by ci.quest_id), ', ') AS procedure_group
         from proc c
         left join client_proc cc on cc.client_id=${req.client_id}
         and cc.proc_id=c.id
         left join lab_company lc on lc.id=c.lab_company_id
-        left join user u on u.id=cc.updated_user_id
+        left join users u on u.id=cc.updated_user_id
         left join proc_item ci on ci.proc_id=c.id
         left join client cl on cl.id=c.client_id
-        where 1 \n`;
+        where true \n`;
     if (procedureId) {
-      $sql += `and c.id like '%${procedureId}%' \n`;
+      $sql += `and c.id ilike '%${procedureId}%' \n`;
     }
     if (procedureDescription) {
-      $sql += `and c.name like '%${procedureDescription}%' \n`;
+      $sql += `and c.name ilike '%${procedureDescription}%' \n`;
     }
     if (labCompanyId) {
       $sql += `and c.lab_company_id=${labCompanyId} \n`;
@@ -77,17 +74,16 @@ const search = async (req, res) => {
     $sql += `limit 50 \n`;
 
     const dbResponse = await db.query($sql);
-    if (!dbResponse) {
+    if (!dbResponse || dbResponse.rows.length === 0) {
       errorMessage.message = "None found";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = dbResponse;
+    successMessage.data = dbResponse.rows;
     return res.status(status.created).send(successMessage);
   } catch (error) {
+    console.log(error);
     errorMessage.message = "Select not not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
@@ -97,38 +93,36 @@ const updateClientProcedure = async (req, res) => {
     errorMessage.message = errors.array();
     return res.status(status.error).send(errorMessage);
   }
-  const db = makeDb(configuration, res);
-  const { procedureId, favorite, billable, fee, notes } = req.body;
+  const { procedureId, favorite, billable, fee, notes } = req.body.data;
+  
   let $sql;
   try {
     $sql = `insert into client_proc (client_id, proc_id, favorite, billable, fee, notes, created, created_user_id, updated, updated_user_id )
         values (${req.client_id}, '${procedureId}', ${favorite}, ${billable}, ${fee >= 0 ? fee : null},
           '${notes}', now(), ${req.user_id}, now(), ${req.user_id} ) 
-         on duplicate key update 
-            favorite=${favorite},
-            billable=${billable},
-            fee=${fee >= 0 ? fee : null},
-            notes='${notes}',
-            created=now(),
-            created_user_id=${req.user_id},
-            updated=now(),
-            updated_user_id=${req.user_id}`;
+          ON CONFLICT(client_id, proc_id) DO UPDATE 
+          SET favorite=$1,
+              billable=$2,
+              fee=${fee >= 0 ? fee : null},
+              notes=$3,
+              created=now(),
+              created_user_id=${req.user_id},
+              updated=now(),
+              updated_user_id=${req.user_id} RETURNING client_id, proc_id`      
 
-    const updateResponse = await db.query($sql);
+    const updateResponse = await db.query($sql, [favorite, billable, notes]);
 
-    if (!updateResponse.affectedRows) {
+    if (!updateResponse.rowCount) {
       errorMessage.message = "Update not not successful";
       return res.status(status.notfound).send(errorMessage);
     }
-    successMessage.data = updateResponse;
+    successMessage.data = updateResponse.rows;
     successMessage.message = "Update successful";
     return res.status(status.success).send(successMessage);
   } catch (error) {
     console.log(error);
     errorMessage.message = "Update not successful";
     return res.status(status.error).send(errorMessage);
-  } finally {
-    await db.close();
   }
 };
 
