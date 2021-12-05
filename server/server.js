70e6732b7333295b17be9f7895889f17f7edb21b
jve0902 <jay090218@gmail.com>
2021-12-05 13:30:28 -0800

    .

--- server/server.js
+++ server/server.js
@@ -95,9 +95,6 @@ app.use(baseAPIPath, require("./app/routes/corporate/login.routes"));
 app.use(baseAPIPath, require("./app/routes/corporate/myself.routes"));
 app.use(baseAPIPath, require("./app/routes/corporate/password-reset.routes"));
 
-// ?
-app.use(baseAPIPath, require("./app/routes/test-postgres.routes"));
-
 app.listen(config.port).on("listening", () => {
   console.log(`API is live on ${config.port}`);
 });
