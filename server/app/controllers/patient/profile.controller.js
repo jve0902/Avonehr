1218e66c4380ad68dd0c9a70826d06fc0802a469
jve0902 <jay090218@gmail.com>
2021-12-21 07:47:22 +0600

    Fix phone other

--- server/app/controllers/patient/profile.controller.js
+++ server/app/controllers/patient/profile.controller.js
@@ -39,7 +39,7 @@ const getPatient = async (req, res) => {
 const updatePatient = async (req, res) => {
 
   const { firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work,
-    phone_note, admin_note, medical_note, address, address2, city, postal, state, height, waist, weight,
+    phone_note, phone_other, admin_note, medical_note, address, address2, city, postal, state, ssn, height, waist, weight,
     insurance_name, insurance_group, insurance_member, insurance_phone, insurance_desc } = req.body.data;
   const formData = req.body.data;
   formData.created = new Date();
@@ -56,12 +56,12 @@ const updatePatient = async (req, res) => {
 
     const updateResponse = await db.query(
       `update patient set firstname=$1, middlename=$2, lastname=$3, email=$4, gender=$5, dob=$6, preferred_name=$7,
-      referred_by=$8, phone_home=$9, phone_cell=$10, phone_work=$11, phone_note=$12, admin_note=$13, medical_note=$14, address=$15,
-      address2=$16, city=$17, postal=$18, state=$19, height=$20, waist=$21, weight=$22, insurance_name=$23, insurance_group=$24, insurance_member=$25,
-      insurance_phone=$26, insurance_desc=$27, updated=now(), updated_user_id=${req.user_id}
+      referred_by=$8, phone_home=$9, phone_cell=$10, phone_work=$11, phone_note=$12, phone_other=$13, admin_note=$14, medical_note=$15, address=$16,
+      address2=$17, city=$18, postal=$19, state=$20, ssn=$21, height=$22, waist=$23, weight=$24, insurance_name=$25, insurance_group=$26, insurance_member=$27,
+      insurance_phone=$28, insurance_desc=$29, updated=now(), updated_user_id=${req.user_id}
       where id=${req.user_id} RETURNING id`,
-      [firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work, phone_note,
-        admin_note, medical_note, address, address2, city, postal, state, height, waist, weight, insurance_name, insurance_group, insurance_member, insurance_phone, insurance_desc]
+      [firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work, phone_note, phone_other,
+        admin_note, medical_note, address, address2, city, postal, state, ssn, height, waist, weight, insurance_name, insurance_group, insurance_member, insurance_phone, insurance_desc]
     );
     if (!updateResponse.rowCount) {
       errorMessage.message = "Update not successful";
@@ -77,12 +77,12 @@ const updatePatient = async (req, res) => {
 
     // Log into patient_history
     await db.query(`insert into patient_history(id, firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work,
-      phone_note, admin_note, medical_note, address, address2, city, postal, state, height, waist, weight, insurance_name,
+      phone_note, phone_other, admin_note, medical_note, address, address2, city, postal, state, ssn, height, waist, weight, insurance_name,
       insurance_group, insurance_member, insurance_phone, insurance_desc, created, created_user_id) 
       VALUES(${req.user_id}, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19,
-        $20, $21, $22, $23, $24, $25, $26, $27, now(), ${req.user_id})`,
-       [firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work, phone_note,
-        admin_note, medical_note, address, address2, city, postal, state, height, waist, weight, insurance_name,
+        $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, now(), ${req.user_id})`,
+       [firstname, middlename, lastname, email, gender, dob, preferred_name, referred_by, phone_home, phone_cell, phone_work, phone_note, phone_other,
+        admin_note, medical_note, address, address2, city, postal, state, ssn, height, waist, weight, insurance_name,
         insurance_group, insurance_member, insurance_phone, insurance_desc]);
 
     successMessage.data = updateResponse.rows;
