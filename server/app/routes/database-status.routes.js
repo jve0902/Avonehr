20000fbf26a3c43074a9e95157f6ef67c4613c5c
jve0902 <jay090218@gmail.com>
2022-03-20 21:01:47 +0100

    Remove login requirement

--- server/app/routes/database-status.routes.js
+++ server/app/routes/database-status.routes.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { authJwt } = require("../middlewares");
 const StatusController = require("../controllers/database-status.controller");
 
 const router = express.Router();
 
 router.get(
   "/database-status",
-  [authJwt.verifyToken],
   StatusController.getDatabaseStatus
 );
 
